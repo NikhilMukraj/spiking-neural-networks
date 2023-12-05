bb2d1289b434d5c086c39e670df381e018b6de27
Nikhiljag <nikhilmukraj@gmail.com>
2023-12-05 18:55:22 -0500

    fixed pearson's correlation

--- src/correlation/mod.rs
+++ src/correlation/mod.rs
@@ -1,14 +1,13 @@
-use std::io::{Error, ErrorKind};
+use std::io::{Result, Error, ErrorKind};
 
 
-fn mean(values: Vec<f64>) -> f64 {
-    values.sum() / values.len()
+fn mean(values: &Vec<f64>) -> f64 {
+    values.iter().sum::<f64>() / values.len() as f64
 }
 
 fn std(values: &Vec<f64>, values_mean: f64) -> f64 {
     values.iter()
         .map(|i| (i - values_mean).powf(2.0))
-        .collect::<Vec<f64>>()
         .sum()
 }
 
@@ -16,19 +15,18 @@ fn pearsonr(x: &Vec<f64>, y: &Vec<f64>) -> Result<f64> {
     if x.len() != y.len() {
         return Err(Error::new(ErrorKind::InvalidInput, "x length must match y length"));
     }
+    
+    let x_mean: f64 = mean(x);
+    let y_mean: f64 = mean(y);
 
-    let x_mean = x.sum() / x.len();
-    let y_mean = y.sum() / x.len();
-
-    let numerator = x.iter().zip(y.iter())
-        .map(|i, j| (i - x_mean) * (j - y_mean()))
-        .collect::<Vec<f64>>()
+    let numerator: f64 = x.iter().zip(y.iter())
+        .map(|(i, j)| (i - x_mean) * (j - y_mean))
         .sum();
 
-    let x_std = std(x, x_mean);
-    let y_std = std(y, y_mean);
+    let x_std: f64 = std(x, x_mean);
+    let y_std: f64 = std(y, y_mean);
 
-    let denominator = x_std * y_std;
+    let denominator: f64 = (x_std * y_std).powf(0.5);
 
-    Ok(numerator / denominator)
+    Ok(numerator / denominator) // returns nan x_std or y_std is 0
 }
