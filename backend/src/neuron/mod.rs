653843d7148e811844f513e4275380449c4f7330
Nikhiljag <nikhilmukraj@gmail.com>
2024-03-31 14:48:46 -0400

    start of peak detection code

--- backend/src/neuron/mod.rs
+++ backend/src/neuron/mod.rs
@@ -1,7 +1,7 @@
 use std::{
+    f64::consts::E, 
     fs::File, 
-    io::{Result, Error, ErrorKind, Write, BufWriter}, 
-    f64::consts::E,
+    io::{BufWriter, Error, ErrorKind, Result, Write}
 };
 use rand::Rng;
 #[path = "../distribution/mod.rs"]
@@ -1268,7 +1268,7 @@ impl HodgkinHuxleyCell {
         bayesian: bool, 
         iterations: usize, 
         filename: &str, 
-        full: bool
+        full: bool,
     ) {
         let mut file = BufWriter::new(File::create(filename)
             .expect("Unable to create file"));
@@ -1276,14 +1276,6 @@ impl HodgkinHuxleyCell {
             writeln!(file, "voltage").expect("Unable to write to file");
             writeln!(file, "{}", self.current_voltage).expect("Unable to write to file");
         } else {
-            // writeln!(file, "voltage,m,n,h").expect("Unable to write to file");
-            // writeln!(file, "{}, {}, {}, {}", 
-            //     self.current_voltage, 
-            //     self.m.state, 
-            //     self.n.state, 
-            //     self.h.state,
-            // ).expect("Unable to write to file");
-
             write!(file, "voltage,m,n,h").expect("Unable to write to file");
             writeln!(
                 file, 
@@ -1328,13 +1320,6 @@ impl HodgkinHuxleyCell {
             if !full {
                 writeln!(file, "{}", self.current_voltage).expect("Unable to write to file");
             } else {
-                // writeln!(file, "{}, {}, {}, {}", 
-                //     self.current_voltage, 
-                //     self.m.state, 
-                //     self.n.state, 
-                //     self.h.state,
-                // ).expect("Unable to write to file");
-
                 write!(file, "{}, {}, {}, {}", 
                     self.current_voltage, 
                     self.m.state, 
@@ -1352,4 +1337,74 @@ impl HodgkinHuxleyCell {
             }
         }
     }
+
+    // pub fn peaks_test(
+    //     &mut self, 
+    //     input: f64, 
+    //     bayesian: bool, 
+    //     iterations: usize, 
+    //     filename: &str, 
+    // ) {
+    //     let mut file = BufWriter::new(File::create(filename)
+    //         .expect("Unable to create file"));
+        
+    //     let mut voltages: Vec<f64> = vec![self.current_voltage];
+
+    //     for _ in 0..iterations {
+    //         if bayesian {
+    //             self.iterate(
+    //                 input * limited_distr(
+    //                     self.bayesian_params.mean, 
+    //                     self.bayesian_params.std, 
+    //                     self.bayesian_params.min, 
+    //                     self.bayesian_params.max,
+    //                 )
+    //             );
+    //         } else {
+    //             self.iterate(input);
+    //         }
+
+    //         voltages.push(self.current_voltage);
+    //     }
+
+    //     let peaks = find_peaks(voltages);
+
+    //     writeln!(file, "voltages,peak").expect("Could not write to file");
+    //     for (n, i) in voltages.iter().enumerate() {
+    //         let is_peak: &str = if peaks[n+2][1] {
+    //             "true"
+    //         } else {
+    //             "false"
+    //         };
+
+    //         writeln!(file, "{}, {}", i, is_peak);
+    //     }
+    // }
 }
+
+// find peaks of hodgkin huxley
+// result starts at index 1 of input list
+// fn diff(x: &Vec<f64>) -> Vec<f64> {
+//     (1..x.len()).map(|i| x[i] - x[i-1])
+//         .collect()
+// }
+
+// fn find_peaks(voltages: Vec<f64>, tolerance: f64) -> Vec<(usize, f64)> {
+//     let first_diff: Vec<f64> = diff(&voltages);
+//     let second_diff: Vec<f64> = diff(&first_diff);
+
+//     let local_optima = first_diff.iter()
+//         .filter(|i| **i <= tolerance)
+//         .map(|i| *i)
+//         .collect::<Vec<f64>>();
+
+//     let local_maxima = local_optima.iter()
+//         .map(|i| *i)
+//         .enumerate()
+//         .filter(|(n, _)| *n < first_diff.len() - 1 && second_diff[n+1] < 0.)
+//         .collect::<Vec<(usize, f64)>>();
+
+//     local_maxima.iter()
+//         .map(|(n, i)| (n + 2, *i))
+//         .collect()
+// }
