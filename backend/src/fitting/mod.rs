c518417ba8fe9e8872d35ec8d37be260cb45d9ab
Nikhiljag <nikhilmukraj@gmail.com>
2024-04-15 14:18:38 -0400

    reference scaling

--- backend/src/fitting/mod.rs
+++ backend/src/fitting/mod.rs
@@ -11,15 +11,14 @@ use crate::neuron::{
 use crate::ga::{BitString, decode};
 
 
-fn get_average_spike(peaks: &Vec<usize>, voltages: &Vec<f64>) -> f64 {
-    let average = peaks.iter()
-        .map(|n| voltages[*n])
-        .sum::<f64>() / (peaks.len() as f64);
-
-    match average {
-        x if x.is_nan() => f64::INFINITY,
-        _ => average,
+fn get_average_spike(peaks: &Vec<usize>, voltages: &Vec<f64>, default: f64) -> f64 {
+    if peaks.len() == 0 {
+        return default;
     }
+
+    peaks.iter()
+        .map(|n| voltages[*n])
+        .sum::<f64>() / (peaks.len() as f64)
 }
 
 #[derive(Debug)]
@@ -28,8 +27,8 @@ pub struct ActionPotentialSummary {
     pub average_post_spike_amplitude: f64,
     pub average_pre_spike_time_difference: f64,
     pub average_post_spike_time_difference: f64,
-    // pub num_pre_spikes: f64,
-    // pub num_post_spikes: f64,
+    pub num_pre_spikes: f64,
+    pub num_post_spikes: f64,
 }
 
 fn get_summary(
@@ -38,6 +37,7 @@ fn get_summary(
     pre_peaks: Option<&Vec<usize>>,
     post_peaks: Option<&Vec<usize>>,
     tolerance: Option<f64>,
+    spike_amplitude_default: f64,
 ) -> Result<ActionPotentialSummary> {
     let pre_peaks = match (pre_peaks, tolerance) {
         (Some(values), None) | (Some(values), Some(_)) => values.to_owned(),
@@ -64,20 +64,22 @@ fn get_summary(
         },
     };
 
-    let average_pre_spike: f64 = get_average_spike(&pre_peaks, pre_voltages);
-    let average_post_spike: f64 = get_average_spike(&post_peaks, post_voltages);
+    let average_pre_spike: f64 = get_average_spike(&pre_peaks, pre_voltages, spike_amplitude_default);
+    let average_post_spike: f64 = get_average_spike(&post_peaks, post_voltages, spike_amplitude_default);
 
-    let mut average_pre_spike_difference: f64 = diff(&pre_peaks).iter()
-        .sum::<usize>() as f64 / (pre_peaks.len() as f64);
-    let mut average_post_spike_difference: f64 = diff(&post_peaks).iter()
-        .sum::<usize>() as f64 / (post_peaks.len() as f64);
+    let average_pre_spike_difference: f64 = if pre_peaks.len() != 0 {
+        diff(&pre_peaks).iter()
+            .sum::<usize>() as f64 / (pre_peaks.len() as f64)
+    } else {
+        0.
+    };
 
-    if average_pre_spike_difference.is_nan() {
-        average_pre_spike_difference = f64::INFINITY;
-    }
-    if average_post_spike_difference.is_nan() {
-        average_post_spike_difference = f64::INFINITY;
-    }
+    let average_post_spike_difference: f64 = if post_peaks.len() != 0 {
+        diff(&post_peaks).iter()
+            .sum::<usize>() as f64 / (post_peaks.len() as f64)
+    } else {
+        0.
+    };
 
     Ok(
         ActionPotentialSummary {
@@ -85,62 +87,93 @@ fn get_summary(
             average_post_spike_amplitude: average_post_spike,
             average_pre_spike_time_difference: average_pre_spike_difference,
             average_post_spike_time_difference: average_post_spike_difference,
+            num_pre_spikes: pre_peaks.len() as f64,
+            num_post_spikes: post_peaks.len() as f64,
         }
     )
 }
 
-// struct SummaryScalingFactors {
-//     amplitude_scale: f64,
-//     time_difference_scale: f64,
-//     // num_peaks_scale: f64,
-// }
+pub struct SummaryScalingDefaults {
+    pub default_amplitude_scale: f64,
+    pub default_time_difference_scale: f64,
+    pub default_num_peaks_scale: f64,
+}
 
-// fn get_f64_max(x: &Vec<f64>) -> Option<&f64> {
-//     x.iter()
-//         .max_by(|a, b| a.total_cmp(b))
-// }
+#[derive(Clone, Copy)]
+pub struct SummaryScalingFactors {
+    pub amplitude_scale: f64,
+    pub time_difference_scale: f64,
+    pub num_peaks_scale: f64,
+}
 
-// fn reference_scale(
-//     reference_summary: &ActionPotentialSummary, 
-// ) -> (ActionPotentialSummary, SummaryScalingFactors) {
-//     let amplitudes = vec![
-//         reference_summary.average_pre_spike_amplitude, reference_summary.average_post_spike_amplitude
-//     ];
+fn get_f64_max(x: &Vec<f64>) -> Option<&f64> {
+    x.iter()
+        .max_by(|a, b| a.total_cmp(b))
+}
 
-//     let time_differences = vec![
-//         reference_summary.average_pre_spike_time_difference, reference_summary.average_post_spike_time_difference
-//     ];
+fn replace_with_default(value: f64, default: f64) -> f64 {
+    if value == 0. {
+        default
+    } else {
+        value
+    }
+}
 
-//     let amplitude_scale = *get_f64_max(&amplitudes).unwrap();
+pub fn get_reference_scale(
+    reference_summary: &ActionPotentialSummary, 
+    scaling_defaults: &SummaryScalingDefaults,
+) -> (ActionPotentialSummary, SummaryScalingFactors) {
+    let amplitudes = vec![
+        reference_summary.average_pre_spike_amplitude, reference_summary.average_post_spike_amplitude
+    ];
+    let time_differences = vec![
+        reference_summary.average_pre_spike_time_difference, reference_summary.average_post_spike_time_difference
+    ];
+    let peaks_lens = vec![
+        reference_summary.num_pre_spikes, reference_summary.num_post_spikes,
+    ];
 
-//     let time_difference_scale = *get_f64_max(&time_differences).unwrap();
+    let amplitude_scale = replace_with_default(
+        *get_f64_max(&amplitudes).unwrap(), scaling_defaults.default_amplitude_scale
+    );
+    let time_difference_scale = replace_with_default(
+        *get_f64_max(&time_differences).unwrap(), scaling_defaults.default_time_difference_scale
+    );
+    let num_peaks_scale = replace_with_default(
+        *get_f64_max(&peaks_lens).unwrap(), scaling_defaults.default_num_peaks_scale
+    );
 
-//     let scaled_reference = ActionPotentialSummary {
-//         average_post_spike_amplitude: reference_summary.average_pre_spike_amplitude / amplitude_scale,
-//         average_pre_spike_amplitude: reference_summary.average_post_spike_amplitude / amplitude_scale,
-//         average_pre_spike_time_difference: reference_summary.average_pre_spike_time_difference / time_difference_scale,
-//         average_post_spike_time_difference: reference_summary.average_post_spike_time_difference / time_difference_scale,
-//     };
+    let scaled_reference = ActionPotentialSummary {
+        average_post_spike_amplitude: reference_summary.average_pre_spike_amplitude / amplitude_scale,
+        average_pre_spike_amplitude: reference_summary.average_post_spike_amplitude / amplitude_scale,
+        average_pre_spike_time_difference: reference_summary.average_pre_spike_time_difference / time_difference_scale,
+        average_post_spike_time_difference: reference_summary.average_post_spike_time_difference / time_difference_scale,
+        num_pre_spikes: reference_summary.num_pre_spikes / num_peaks_scale,
+        num_post_spikes: reference_summary.num_post_spikes / num_peaks_scale,
+    };
 
-//     let scaling_factors = SummaryScalingFactors {
-//         amplitude_scale: amplitude_scale, 
-//         time_difference_scale: time_difference_scale,
-//     };
+    let scaling_factors = SummaryScalingFactors {
+        amplitude_scale: amplitude_scale, 
+        time_difference_scale: time_difference_scale,
+        num_peaks_scale: num_peaks_scale,
+    };
 
-//     (scaled_reference, scaling_factors)
-// }
+    (scaled_reference, scaling_factors)
+}
 
-// fn scale_summary(
-//     summary: &ActionPotentialSummary, 
-//     scaling_factors: &SummaryScalingFactors
-// ) -> ActionPotentialSummary {
-//     ActionPotentialSummary {
-//         average_post_spike_amplitude: summary.average_pre_spike_amplitude / scaling_factors.amplitude_scale,
-//         average_pre_spike_amplitude: summary.average_post_spike_amplitude / scaling_factors.amplitude_scale,
-//         average_pre_spike_time_difference: summary.average_pre_spike_time_difference / scaling_factors.time_difference_scale,
-//         average_post_spike_time_difference: summary.average_post_spike_time_difference / scaling_factors.time_difference_scale,
-//     }
-// }
+fn scale_summary(
+    summary: &ActionPotentialSummary, 
+    scaling_factors: &SummaryScalingFactors
+) -> ActionPotentialSummary {
+    ActionPotentialSummary {
+        average_post_spike_amplitude: summary.average_pre_spike_amplitude / scaling_factors.amplitude_scale,
+        average_pre_spike_amplitude: summary.average_post_spike_amplitude / scaling_factors.amplitude_scale,
+        average_pre_spike_time_difference: summary.average_pre_spike_time_difference / scaling_factors.time_difference_scale,
+        average_post_spike_time_difference: summary.average_post_spike_time_difference / scaling_factors.time_difference_scale,
+        num_pre_spikes: summary.num_pre_spikes / scaling_factors.num_peaks_scale,
+        num_post_spikes: summary.num_post_spikes / scaling_factors.num_peaks_scale,
+    }
+}
 
 fn compare_summary(summary1: &ActionPotentialSummary, summary2: &ActionPotentialSummary) -> f64 {
     let pre_spike_amplitude = (summary1.average_pre_spike_amplitude - summary2.average_pre_spike_amplitude).powf(2.);
@@ -149,7 +182,12 @@ fn compare_summary(summary1: &ActionPotentialSummary, summary2: &ActionPotential
     let pre_spike_difference = (summary1.average_pre_spike_time_difference - summary2.average_pre_spike_time_difference).powf(2.);
     let post_spike_difference = (summary1.average_post_spike_time_difference - summary2.average_post_spike_time_difference).powf(2.);
 
-    let output = pre_spike_amplitude + post_spike_amplitude + pre_spike_difference + post_spike_difference;
+    let num_pre_spikes = (summary1.num_pre_spikes - summary2.num_pre_spikes).powf(2.);
+    let num_post_spikes = (summary1.num_post_spikes - summary2.num_post_spikes).powf(2.);
+
+    let output = pre_spike_amplitude + post_spike_amplitude + 
+        pre_spike_difference + post_spike_difference +
+        num_pre_spikes + num_post_spikes;
 
     if output.is_nan() {
         f64::INFINITY
@@ -163,7 +201,8 @@ pub fn get_hodgkin_huxley_voltages(
     input_current: f64, 
     iterations: usize,
     bayesian: bool, 
-    tolerance: f64
+    tolerance: f64,
+    spike_amplitude_default: f64,
 ) -> Result<ActionPotentialSummary> {
     let mut presynaptic_neuron = hodgkin_huxley_model.clone();
     let mut postsynaptic_neuron = hodgkin_huxley_model.clone();
@@ -203,7 +242,11 @@ pub fn get_hodgkin_huxley_voltages(
         post_voltages.push(postsynaptic_neuron.current_voltage);
     }
 
-    Ok(get_summary(&pre_voltages, &post_voltages, None, None, Some(tolerance))?)
+    Ok(
+        get_summary(
+            &pre_voltages, &post_voltages, None, None, Some(tolerance), spike_amplitude_default
+        )?
+    )
 }
 
 #[derive(Clone)]
@@ -211,7 +254,8 @@ pub struct FittingSettings<'a> {
     pub hodgkin_huxley_model: HodgkinHuxleyCell,
     pub if_params: &'a IFParameters,
     pub action_potential_summary: &'a ActionPotentialSummary,
-    // pub scaling_factors: &'a SummaryScalingFactors,
+    pub scaling_factors: Option<SummaryScalingFactors>,
+    pub spike_amplitude_default: f64,
     pub input_current: f64,
     pub iterations: usize,
     pub bayesian: bool,
@@ -288,9 +332,14 @@ pub fn get_izhikevich_summary(
         post_voltages.push(postsynaptic_neuron.current_voltage);
     }
 
-    get_summary(
-        &pre_voltages, &post_voltages, Some(&pre_peaks), Some(&post_peaks), None
-    )
+    let summary = get_summary(
+        &pre_voltages, &post_voltages, Some(&pre_peaks), Some(&post_peaks), None, settings.spike_amplitude_default
+    )?;
+
+    match settings.scaling_factors {
+        Some(factors) => Ok(scale_summary(&summary, &factors)),
+        None => Ok(summary),
+    }
 }
 
 // bounds should be a, b, c, d, and v_th for now
@@ -346,7 +395,7 @@ pub fn fitting_objective(
         &mut presynaptic_neuron, 
         &mut postsynaptic_neuron, 
         &if_params,
-        settings
+        settings,
     )?;
 
     Ok(compare_summary(&summary, settings.action_potential_summary))
