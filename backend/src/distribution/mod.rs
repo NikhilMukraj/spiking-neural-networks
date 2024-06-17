4bb7ec93b9b8db6a11a19165e642ad32f408d3a7
Nikhiljag <nikhilmukraj@gmail.com>
2024-06-17 14:09:24 -0400

    distribution docs

--- backend/src/distribution/mod.rs
+++ backend/src/distribution/mod.rs
@@ -1,12 +1,17 @@
+//! A tool to generate and clamp noise.
+
 use rand_distr::{Normal, Distribution};
 
 
-pub fn limited_distr(mean: f64, std_dev: f64, minimum: f64, maximum: f64) -> f64 {
-    if std_dev == 0.0 {
+/// Calculates the normal distribution at the given mean and standard deviation and clamps
+/// the output value between the given minimum and maximum, if standard deviation is `0.` the 
+/// mean is always returned
+pub fn limited_distr(mean: f64, std: f64, minimum: f64, maximum: f64) -> f64 {
+    if std == 0.0 {
         return mean;
     }
 
-    let normal = Normal::new(mean, std_dev).unwrap();
+    let normal = Normal::new(mean, std).unwrap();
     let output: f64 = normal.sample(&mut rand::thread_rng());
    
     output.max(minimum).min(maximum)
