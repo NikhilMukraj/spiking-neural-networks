ebc3bd3b4b3d958c7b3b151742a1699936902235
Nikhiljag <nikhilmukraj@gmail.com>
2024-06-17 14:09:35 -0400

    eeg tooling

--- backend/src/eeg/mod.rs
+++ backend/src/eeg/mod.rs
@@ -1,5 +1,6 @@
+//! A set of tools to analyze power spectral density for EEG time series.
+
 use std::io::{Result, Error, ErrorKind};
-use csv::Reader;
 use ndarray::{Array1, s};
 use num_complex::Complex;
 use rustfft::{FftPlanner, FftDirection};
@@ -7,6 +8,8 @@ mod emd;
 use emd::earth_moving_distance;
 
 
+/// Retrieves the power density of the given time series based on the given timestep (ms)
+/// and total time elapsed by the end of the series (ms)
 pub fn get_power_density(x: Vec<f64>, dt: f64, total_time: f64) -> (Array1<f64>, Array1<f64>) {
     let x_mean = x.iter().sum::<f64>() / x.len() as f64;
 
@@ -40,63 +43,11 @@ pub fn get_power_density(x: Vec<f64>, dt: f64, total_time: f64) -> (Array1<f64>,
     return (faxis, sxx_positive.to_owned())
 }
 
-pub fn read_eeg_csv(filename: &str) -> Result<(Vec<f64>, f64, f64)> {
-    let mut x: Vec<f64> = Vec::new();
-    let mut reader = Reader::from_path(filename)?;
-
-    let headers = reader.headers()?;
-    let headers = headers
-        .iter()
-        .collect::<Vec<&str>>()[0];
-
-    let header_parsed = headers.split(":").collect::<Vec<&str>>();
-    let dt = match header_parsed[0].parse::<f64>() {
-        Ok(parsed_val) => { parsed_val },
-        Err(e) => { return Err(Error::new(ErrorKind::InvalidInput, e.to_string())) },
-    };
-    let total_time = match header_parsed[1].parse::<f64>() {
-        Ok(parsed_val) => { parsed_val },
-        Err(e) => { return Err(Error::new(ErrorKind::InvalidInput, e.to_string())) },
-    };
-    println!("EEG parameters:\ndt: {}\nT: {}", dt, total_time);
-
-    for record in reader.records() {
-        let record = match record {
-            Ok(record_val) => { record_val },
-            Err(e) => { return Err(Error::new(ErrorKind::InvalidInput, e.to_string())) },
-        };
-
-        let record = match record[0].parse::<f64>() {
-            Ok(record_float) => { record_float },
-            Err(e) => { return Err(Error::new(ErrorKind::InvalidInput, e.to_string())) },
-        };
-
-        x.push(record);
-    }
-
-    Ok((x, dt, total_time))
-}
-
-// to be replaced with more detailed metric
-// pub fn power_density_mse(sxx1: &Array1<f64>, sxx2: &Array1<f64>) -> Result<f64> {
-//     if sxx1.len() != sxx2.len() {
-//         return Err(Error::new(ErrorKind::InvalidInput, "Lengths of inputs must match"));
-//     }
-
-//     let mse = sxx1.iter()
-//         .zip(sxx2.iter())
-//         .map(|(x, y)| (x - y).powf(2.0))
-//         .sum();
-
-//     Ok(mse)
-// }
-
 fn find_max(arr: &Array1<f64>) -> Option<&f64> {
     arr.iter().max_by(|a, b| a.total_cmp(b))
 }
 
-// mse isnt great metric
-// this should be more applicable
+/// Compares two power densities using the earth moving distance
 pub fn power_density_comparison(sxx1: &Array1<f64>, sxx2: &Array1<f64>) -> Result<f64> {
     if sxx1.len() != sxx2.len() {
         return Err(Error::new(ErrorKind::InvalidInput, "Lengths of inputs must match"));
