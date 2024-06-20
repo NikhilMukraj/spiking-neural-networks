2cc49ff40cf3e16f884d18955afdaf82de7fe892
Nikhiljag <nikhilmukraj@gmail.com>
2024-06-20 14:29:09 -0400

    start of hopfield example

---
+++ backend/examples/hopfield_example/main.rs
@@ -0,0 +1,108 @@
+use std::{
+    env,
+    fs::{read_to_string, File}, 
+    io::{BufWriter, Error, ErrorKind, Result, Write}
+};
+extern crate spiking_neural_networks;
+use crate::spiking_neural_networks::{
+    neuron::attractors::{
+        DiscreteNeuronLattice, generate_hopfield_network, distort_pattern,
+    },
+    graph::{GraphFunctionality, AdjacencyMatrix}
+};
+
+
+fn read_pattern(file_contents: &str) -> Result<Vec<Vec<isize>>> {
+    let mut matrix = Vec::new();
+
+    for line in file_contents.split("\n") {
+        let row: Vec<isize> = line.split(',')
+            .map(|s| s.trim().parse().expect("Could not parse pattern"))
+            .collect();
+
+        if row.iter().any(|i| *i != -1 && *i != 1) {
+            return Err(Error::new(ErrorKind::InvalidData, "Pattern must be bipolar (-1 or 1)"))
+        }
+
+        matrix.push(row);
+    }
+
+    Ok(matrix)
+}
+
+fn test_hopfield_network<T: GraphFunctionality>(
+    patterns: &Vec<Vec<Vec<isize>>>,
+    noise_level: f64,
+    iterations: usize,
+) -> Result<()> {
+    let num_rows = patterns[0].len();
+    let num_cols = patterns[0][0].len();
+
+    let weights = generate_hopfield_network::<T>(0, &patterns)?;
+    let mut discrete_lattice = DiscreteNeuronLattice::<T>::generate_lattice_from_dimension(num_rows, num_cols);
+    discrete_lattice.weights = weights;
+
+    for (n, pattern) in patterns.iter().enumerate() {
+        let distorted_pattern = distort_pattern(&pattern, noise_level);
+
+        let mut hopfield_history: Vec<Vec<Vec<isize>>> = Vec::new();
+
+        discrete_lattice.input_pattern_into_discrete_grid(distorted_pattern);
+        hopfield_history.push(discrete_lattice.convert_to_numerics());
+
+        for _ in 0..iterations {
+            discrete_lattice.iterate()?;
+            hopfield_history.push(discrete_lattice.convert_to_numerics());
+        }
+
+        let mut hopfield_file = BufWriter::new(File::create(format!("{}_hopfield.txt", n))
+            .expect("Could not create file"));
+
+        for grid in hopfield_history {
+            for row in grid {
+                for value in row {
+                    write!(hopfield_file, "{} ", value)
+                        .expect("Could not write to file");
+                }
+                writeln!(hopfield_file)
+                    .expect("Could not write to file");
+            }
+            writeln!(hopfield_file, "-----")
+                .expect("Could not write to file"); 
+        }
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let pattern_files: Vec<String> = env::args().skip(1).collect();
+
+    let patterns: Vec<Vec<Vec<isize>>> = pattern_files.iter()
+        .map(|i| 
+            read_pattern(
+                &(read_to_string(i).expect(&format!("Could not read file: {}", i)))
+            ).expect("Pattern could not be read, must be bipolar (-1 or 1 for every value)")
+        )
+        .collect();
+
+    let num_rows = patterns[0].len();
+    let num_cols = patterns[0][0].len();
+
+    for pattern in patterns.iter() {
+        if pattern.len() != num_rows {
+            return Err(Error::new(ErrorKind::InvalidInput, "Patterns must have the same size"));
+        }
+
+        if pattern.iter().any(|row| row.len() != num_cols) {
+            return Err(Error::new(ErrorKind::InvalidInput, "Patterns must have the same size"));
+        }
+    }
+
+    let iterations = 10;
+    let noise_level = 0.25;
+
+    test_hopfield_network::<AdjacencyMatrix>(&patterns, noise_level, iterations)?;
+
+    Ok(())
+}
