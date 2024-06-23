bac5cbc4f8ef9d207b96660e5dc4489b815a644d
Nikhiljag <nikhilmukraj@gmail.com>
2024-06-23 17:14:05 -0400

    image examples

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -8,7 +8,29 @@
 //! plasticity, basic attractors and dynamics for neurons connected in a lattice. 
 //! See below for examples and how to add custom models.
 //! 
-//! ![Voltage over time](https://github.com/NikhilMukraj/spiking-neural-networks/blob/main/backend/examples/images/lattice.gif?raw=true)
+//! ### FitzHugh-Nagumo Model with Static Input
+//! 
+//! ![FitzHugh-Nagumo with static current input](https://github.com/NikhilMukraj/spiking-neural-networks/blob/main/backend/docs/images/fhn_static.png?raw=true)
+//! 
+//! ### Coupled Izhikevich Neurons
+//! 
+//! ![Coupled Izhikevich models](https://github.com/NikhilMukraj/spiking-neural-networks/blob/main/backend/docs/images/izhikevich.png?raw=true)
+//! 
+//! ### Hodgkin Huxley Model with Neurotransmission
+//! 
+//! ![Hodgkin Huxley model voltage and neurotransmitter over time](https://github.com/NikhilMukraj/spiking-neural-networks/blob/main/backend/docs/images/hodgkin_huxley.png?raw=true)
+//! 
+//! ### Spike Time Dependent Plasticity Weights over Time
+//! 
+//! ![STDP weights over time](https://github.com/NikhilMukraj/spiking-neural-networks/blob/main/backend/docs/images/stdp.png?raw=true)
+//! 
+//! ### Hopfield Network Pattern Reconstruction
+//! 
+//! ![Discrete Hopfield network pattern reconstruction](https://github.com/NikhilMukraj/spiking-neural-networks/blob/main/backend/docs/images/hopfield.png?raw=true)
+//! 
+//! ### Lattice
+//! 
+//! ![Voltage over time](https://github.com/NikhilMukraj/spiking-neural-networks/blob/main/backend/docs/images/lattice.gif?raw=true)
 //!
 //! ## Example Code:
 //! 
