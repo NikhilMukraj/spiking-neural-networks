0ba179eed1f7e32f6acd0241eb9ccd238f16a7f6
Nikhiljag <nikhilmukraj@gmail.com>
2024-06-26 14:37:46 -0400

    start of lattice network

---
+++ backend/examples/lattice_network_example/main.rs
@@ -0,0 +1,18 @@
+// extern crate spiking_neural_networks;
+// use spiking_neural_networks::{graph::Graph, neuron::{
+//     integrate_and_fire::IzhikevichNeuron, Lattice
+// }};
+
+
+// fn connection_conditional(x: (usize, usize), y: (usize, usize)) -> bool {
+//     x == y
+// }
+
+fn main() {
+    // lattice network test
+    // set up 3x3 poisson grid and 3x3 izhikevich grid
+    // poisson should be presynaptic and connect to one other izhikevich neuron
+    // izhikevich neuron should have no postsynaptic connections
+    // test by first setting poisson firing rate to 0, neurons should not spike often
+    // then set poisson firing rate to something higher, neurons should then spike more
+}
