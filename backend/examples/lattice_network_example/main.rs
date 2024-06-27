46f035fe267b56956271f75c023eb68745de7862
Nikhiljag <nikhilmukraj@gmail.com>
2024-06-27 14:23:18 -0400

    start of lattice network testing

--- backend/examples/lattice_network_example/main.rs
+++ backend/examples/lattice_network_example/main.rs
@@ -15,4 +15,6 @@ fn main() {
     // izhikevich neuron should have no postsynaptic connections
     // test by first setting poisson firing rate to 0, neurons should not spike often
     // then set poisson firing rate to something higher, neurons should then spike more
+    // if timestep == iterations / 2 change firing rate
+    // izhikevich_lattice.update_history = true;
 }
