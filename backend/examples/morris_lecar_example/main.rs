d427c7cd126304b33e3ad3f2f290cd015e69b8e0
Nikhiljag <nikhilmukraj@gmail.com>
2024-07-06 17:06:06 -0400

    morris lecar example

---
+++ backend/examples/morris_lecar_example/main.rs
@@ -0,0 +1,35 @@
+use std::{
+    fs::File,
+    io::{BufWriter, Write},
+};
+extern crate spiking_neural_networks;
+use crate::spiking_neural_networks::neuron::{
+    iterate_and_spike::IterateAndSpike,
+    morris_lecar::MorrisLecarNeuron,
+};
+
+
+// Inputs a static current into the neuron model and tracks the voltage 
+// over time, writes the output file to the current working directory
+fn main() {
+    let mut morris_lecar_neuron = MorrisLecarNeuron::default_impl();
+
+    let iterations = 10000;
+    let input = 100.;
+
+    let mut voltages: Vec<f32> = vec![];
+
+    for _ in 0..iterations {
+        morris_lecar_neuron.iterate_and_spike(input);
+
+        voltages.push(morris_lecar_neuron.current_voltage);
+    }
+
+    let mut file = BufWriter::new(File::create("morris_lecar_static_input.csv")
+        .expect("Could not create file"));
+
+    writeln!(file, "voltages").expect("Could not write to file");
+    for i in 0..iterations {
+        writeln!(file, "{}", voltages[i]).expect("Could not write to file");
+    }
+}
