af8258cb9b6692025d0d18673438beee0ef9584b
Nikhiljag <nikhilmukraj@gmail.com>
2024-07-09 20:22:59 -0400

    parser

---
+++ build_test/src/main.rs
@@ -0,0 +1,119 @@
+use std::env;
+use pest::iterators::Pairs;
+use pest::pratt_parser::PrattParser;
+use pest::Parser;
+use std::io::{self, BufRead};
+
+
+#[derive(pest_derive::Parser)]
+#[grammar = "calculator.pest"]
+pub struct CalculatorParser;
+
+lazy_static::lazy_static! {
+    static ref PRATT_PARSER: PrattParser<Rule> = {
+        use pest::pratt_parser::{Assoc::*, Op};
+        use Rule::*;
+
+        PrattParser::new()
+            .op(Op::infix(add, Left) | Op::infix(subtract, Left))
+            .op(Op::infix(multiply, Left) | Op::infix(divide, Left))
+            .op(Op::prefix(unary_minus))
+    };
+}
+
+#[derive(Debug)]
+pub enum Expr {
+    Number(f32),
+    Name(String),
+    UnaryMinus(Box<Expr>),
+    BinOp {
+        lhs: Box<Expr>,
+        op: Op,
+        rhs: Box<Expr>,
+    },
+}
+
+pub fn parse_expr(pairs: Pairs<Rule>) -> Expr {
+    PRATT_PARSER
+        .map_primary(|primary| match primary.as_rule() {
+            Rule::number => Expr::Number(primary.as_str().parse::<f32>().unwrap()),
+            Rule::name => Expr::Name(String::from(primary.as_str())),
+            Rule::expr => parse_expr(primary.into_inner()),
+            rule => unreachable!("Expr::parse expected atom, found {:?}", rule),
+        })
+        .map_infix(|lhs, op, rhs| {
+            let op = match op.as_rule() {
+                Rule::add => Op::Add,
+                Rule::subtract => Op::Subtract,
+                Rule::multiply => Op::Multiply,
+                Rule::divide => Op::Divide,
+                rule => unreachable!("Expr::parse expected infix operation, found {:?}", rule),
+            };
+            Expr::BinOp {
+                lhs: Box::new(lhs),
+                op,
+                rhs: Box::new(rhs),
+            }
+        })
+        .map_prefix(|op, rhs| match op.as_rule() {
+            Rule::unary_minus => Expr::UnaryMinus(Box::new(rhs)),
+            _ => unreachable!(),
+        })
+        .parse(pairs)
+}
+
+#[derive(Debug)]
+pub enum Op {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+}
+
+impl Expr {
+    pub fn to_string(&self) -> String {
+        match self {
+            Expr::Number(n) => n.to_string(),
+            Expr::Name(name) => name.clone(),
+            Expr::UnaryMinus(expr) => format!("-{}", expr.to_string()),
+            Expr::BinOp { lhs, op, rhs } => {
+                let op_str = match op {
+                    Op::Add => "+",
+                    Op::Subtract => "-",
+                    Op::Multiply => "*",
+                    Op::Divide => "/",
+                };
+                format!("({} {} {})", lhs.to_string(), op_str, rhs.to_string())
+            }
+        }
+    }
+}
+
+fn main() -> io::Result<()> {
+    for (key, value) in env::vars() {
+        if key == "FOO" {
+            println!("{}: {}", key, value);
+        }
+    }
+
+    for line in io::stdin().lock().lines() {
+        match CalculatorParser::parse(Rule::equation, &line?) {
+            Ok(mut pairs) => {
+                let current_expr = parse_expr(pairs.next().unwrap().into_inner());
+                println!(
+                    "Parsed: {:#?}\nString: {}",
+                    // inner of expr
+                    current_expr,
+                    current_expr.to_string(),
+                );
+            }
+            Err(e) => {
+                eprintln!("Parse failed: {:?}", e);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+// todo support functions in expression
