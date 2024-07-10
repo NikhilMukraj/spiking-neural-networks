e87bbd093ac9257c533b4d3667bc4963f6553e56
Nikhiljag <nikhilmukraj@gmail.com>
2024-07-10 15:09:02 -0400

    notes

--- build_test/src/main.rs
+++ build_test/src/main.rs
@@ -33,6 +33,8 @@ pub enum Expr {
     },
 }
 
+// check function
+// make sure functions are not recursive
 pub fn parse_expr(pairs: Pairs<Rule>) -> Expr {
     PRATT_PARSER
         .map_primary(|primary| match primary.as_rule() {
