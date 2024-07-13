9c258d741bc26d934353a4e4990fa3a46637aa54
Nikhiljag <nikhilmukraj@gmail.com>
2024-07-13 15:34:55 -0400

    neurotransmitter refactor

--- backend/src/neuron/morris_lecar/mod.rs
+++ backend/src/neuron/morris_lecar/mod.rs
@@ -138,7 +138,7 @@ impl<T: NeurotransmitterKinetics, R: ReceptorKinetics> IterateAndSpike for Morri
     fn iterate_with_neurotransmitter_and_spike(
         &mut self, 
         input_current: f32, 
-        t_total: Option<&NeurotransmitterConcentrations>,
+        t_total: &NeurotransmitterConcentrations,
     ) -> bool {
         self.ligand_gates.update_receptor_kinetics(t_total);
         self.ligand_gates.set_receptor_currents(self.current_voltage);
