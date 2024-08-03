6f0a8aebbb47ecc2142ee610b2d2c3aaecb57b1b
Nikhiljag <nikhilmukraj@gmail.com>
2024-08-03 13:19:40 -0400

    fixed agent type macros

--- backend/src/neuron/mod.rs
+++ backend/src/neuron/mod.rs
@@ -3972,7 +3972,6 @@ where
 #[macro_export]
 #[doc(hidden)]
 macro_rules! raw_create_agent_type_for_lattice {
-    // Default case with no optional parameters
     (
         $name:ident,
         $reward_mod_weight:ty,
@@ -3988,7 +3987,6 @@ macro_rules! raw_create_agent_type_for_lattice {
         >;
     };
 
-    // Case with optional lattice history
     (
         $name:ident,
         $reward_mod_weight:ty,
@@ -4013,7 +4011,6 @@ pub use raw_create_agent_type_for_lattice as create_agent_type_for_lattice;
 #[doc(hidden)]
 #[macro_export]
 macro_rules! raw_create_agent_type_for_network {
-    // Default case with no optional parameters
     (
         $name:ident,
         $plasticity:ty,
@@ -4036,7 +4033,7 @@ macro_rules! raw_create_agent_type_for_network {
         >;
     };
 
-    // Case with optional lattice history
+    
     (
         $name:ident,
         $plasticity:ty,
@@ -4044,7 +4041,7 @@ macro_rules! raw_create_agent_type_for_network {
         $reward_mod_weight:ty,
         $iterate_and_spike:ty,
         $spike_train:ty,
-        lattice_history = $lattice_history:ty $(, spike_train_lattice_history = $spike_train_lattice_history:ty)?
+        lattice_history = $lattice_history:ty,
     ) => {
         type $name = RewardModulatedLatticeNetwork<
             $reward_mod_weight,
@@ -4052,10 +4049,60 @@ macro_rules! raw_create_agent_type_for_network {
             spiking_neural_networks::graph::AdjacencyMatrix<(usize, usize), f32>,
             $lattice_history,
             $spike_train,
-            $crate::type_or_default!(
-                $($spike_train_lattice_history)?, 
-                spiking_neural_networks::neuron::SpikeTrainGridHistory
-            ),
+            spiking_neural_networks::neuron::SpikeTrainGridHistory,
+            spiking_neural_networks::graph::AdjacencyMatrix<
+                spiking_neural_networks::graph::GraphPosition, 
+                spiking_neural_networks::neuron::RewardModulatedConnection<$reward_mod_weight>
+            >,
+            $plasticity,
+            $reward_mod_plasticity,
+            spiking_neural_networks::graph::AdjacencyMatrix<(usize, usize), $reward_mod_weight>
+        >;
+    };
+
+    (
+        $name:ident,
+        $plasticity:ty,
+        $reward_mod_plasticity:ty,
+        $reward_mod_weight:ty,
+        $iterate_and_spike:ty,
+        $spike_train:ty,
+        spike_train_lattice_history = $spike_train_lattice_history:ty
+    ) => {
+        type $name = RewardModulatedLatticeNetwork<
+            $reward_mod_weight,
+            $iterate_and_spike,
+            spiking_neural_networks::graph::AdjacencyMatrix<(usize, usize), f32>,
+            spiking_neural_networks::neuron::GridVoltageHistory,
+            $spike_train,
+            $spike_train_lattice_history,
+            spiking_neural_networks::graph::AdjacencyMatrix<
+                spiking_neural_networks::graph::GraphPosition, 
+                spiking_neural_networks::neuron::RewardModulatedConnection<$reward_mod_weight>
+            >,
+            $plasticity,
+            $reward_mod_plasticity,
+            spiking_neural_networks::graph::AdjacencyMatrix<(usize, usize), $reward_mod_weight>
+        >;
+    };
+
+    (
+        $name:ident,
+        $plasticity:ty,
+        $reward_mod_plasticity:ty,
+        $reward_mod_weight:ty,
+        $iterate_and_spike:ty,
+        $spike_train:ty,
+        lattice_history = $lattice_history:ty,
+        spike_train_lattice_history = $spike_train_lattice_history:ty
+    ) => {
+        type $name = RewardModulatedLatticeNetwork<
+            $reward_mod_weight,
+            $iterate_and_spike,
+            spiking_neural_networks::graph::AdjacencyMatrix<(usize, usize), f32>,
+            $lattice_history,
+            $spike_train,
+            $spike_train_lattice_history,
             spiking_neural_networks::graph::AdjacencyMatrix<
                 spiking_neural_networks::graph::GraphPosition, 
                 spiking_neural_networks::neuron::RewardModulatedConnection<$reward_mod_weight>
