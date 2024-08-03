f02194a5ed410555137dbe62476b29fbe83b37ed
Nikhiljag <nikhilmukraj@gmail.com>
2024-08-03 13:12:43 -0400

    liquid state machine test

--- backend/examples/lsm_architecture_example/main.rs
+++ backend/examples/lsm_architecture_example/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::{BufWriter, Write};
+
 use rand::Rng;
 extern crate spiking_neural_networks;
 use spiking_neural_networks::neuron::create_agent_type_for_network;
@@ -23,7 +26,7 @@ fn sparse_connect(x: (usize, usize), y: (usize, usize)) -> bool {
 }
 
 fn feedforward_connect(x: (usize, usize), y: (usize, usize)) -> bool {
-    y.1 - x.1 == 1 
+    y.0 - x.0 == 1 
 }
 
 create_agent_type_for_network!(
@@ -65,10 +68,10 @@ fn state_encoder(state: &TestState, network: &mut AgentType) {
         network.spike_trains_values_mut()
             .for_each(|i| {
                 i.apply(|n| {
-                    n.chance_of_firing = 0.01
+                    n.chance_of_firing = 0.025
                 })
             })
-    } else if state.timestep % 2000 == 1 || state.timestep == 0 {
+    } else if state.timestep % 2000 == 500 || state.timestep == 0 {
         network.spike_trains_values_mut()
             .for_each(|i| {
                 i.apply(|n| {
@@ -78,16 +81,20 @@ fn state_encoder(state: &TestState, network: &mut AgentType) {
     }
 }
 
+/// Creates a skeleton for a liquid state machine and writes the history
+/// of weights and voltages for the readout layer
 fn main() -> Result<(), SpikingNeuralNetworksError> {
     let base_neuron = IzhikevichNeuron::default_impl();
     let base_spike_train = PoissonNeuron::default_impl();
 
     let mut poisson_input = SpikeTrainLattice::default_impl();
     poisson_input.populate(&base_spike_train, 1, 10);
+    poisson_input.update_grid_history = true;
+    poisson_input.set_id(0);
 
     let mut liquid = Lattice::default_impl();
     liquid.populate(&base_neuron, 10, 10);
-    // weight matrix should have a spectral radius of 1
+    // could edit so weight matrix should have a spectral radius of 1
     liquid.connect(&sparse_connect, None);
     liquid.apply(|n| {
         n.current_voltage = rand::thread_rng().gen_range(n.v_init..=n.v_th);
@@ -95,19 +102,21 @@ fn main() -> Result<(), SpikingNeuralNetworksError> {
     liquid.set_id(1);
 
     let mut readout = RewardModulatedLattice::default_impl();
-    readout.populate(&base_neuron, 4, 3);
+    readout.populate(&base_neuron, 4, 2);
     readout.connect(&feedforward_connect, &(
         |_, _| TraceRSTDP {
             counter: 0,
             dw: 0.,
             c: 0.,
-            weight: rand::thread_rng().gen_range(0.5..=2.0),
+            weight: rand::thread_rng().gen_range(0.1..=0.5),
         }
     ));
     readout.apply(|n| {
         n.current_voltage = rand::thread_rng().gen_range(n.v_init..=n.v_th);
     });
     readout.do_modulation = true;
+    readout.update_graph_history = true;
+    readout.update_grid_history = true;
     readout.set_id(2);
 
     let lattices = vec![liquid];
@@ -121,7 +130,12 @@ fn main() -> Result<(), SpikingNeuralNetworksError> {
     )?;
 
     lsm.connect(0, 1, &(|_, _| rand::thread_rng().gen_range(0 as f32..=1.) < 0.05), None)?;
-    lsm.connect(1, 2, &(|_, y| y.0 == 1 && rand::thread_rng().gen_range(0 as f32..=1.) < 0.05), None)?;
+    lsm.connect_with_reward_modulation(
+        1, 
+        2, 
+        &(|_, y| y.0 == 0 && rand::thread_rng().gen_range(0 as f32..=1.) < 0.05), 
+        &(|_, _| RewardModulatedConnection::Weight(2.0))
+    )?;
 
     let state = TestState { timestep: 0, dopamine_history: vec![] };
     let mut env = Environment { 
@@ -132,6 +146,37 @@ fn main() -> Result<(), SpikingNeuralNetworksError> {
     };
 
     env.run(10000)?;
+
+    let mut weights_file = BufWriter::new(File::create("weights.txt").expect("Could not create file"));
+
+    for matrix in &env.agent.get_reward_modulated_lattice(&2).unwrap().graph.history {
+        for row in matrix {
+            for value in row {
+                match value {
+                    Some(trace) => write!(weights_file, "{},", trace.weight).expect("Could not write to file"),
+                    None => write!(weights_file, "0,").expect("Could not write to file"),
+                }
+            }
+            
+            writeln!(weights_file).expect("Could not write to file");
+        }
+
+        writeln!(weights_file, "-----").expect("Could not write to file");
+    }
+
+    let mut voltage_file = BufWriter::new(File::create("voltage.txt").expect("Could not create file"));
+
+    for matrix in &env.agent.get_reward_modulated_lattice(&2).unwrap().grid_history.history {
+        for row in matrix {
+            for value in row {
+                write!(voltage_file, "{},", value).expect("Could not write to file");
+            }
+            
+            writeln!(voltage_file).expect("Could not write to file");
+        }
+
+        writeln!(voltage_file, "-----").expect("Could not write to file");
+    }
 
     Ok(())
 }
