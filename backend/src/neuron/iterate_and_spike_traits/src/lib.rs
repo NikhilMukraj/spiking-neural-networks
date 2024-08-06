d57bd762fef805ac8197e47282df485c936df9f3
Nikhiljag <nikhilmukraj@gmail.com>
2024-08-06 10:28:37 -0400

    iterate and spike trait changes

--- backend/src/neuron/iterate_and_spike_traits/src/lib.rs
+++ backend/src/neuron/iterate_and_spike_traits/src/lib.rs
@@ -89,3 +89,111 @@ pub fn derive_timestep_trait(input: TokenStream) -> TokenStream {
 
     TokenStream::from(expanded)
 }
+
+/// Derive macro to automatically implement the `CurrentVoltage` trait assuming the 
+/// struct has field `current_voltage`
+#[proc_macro_derive(CurrentVoltage)]
+pub fn derive_current_voltage_trait(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let name = input.ident;
+
+    let generics = &input.generics;
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+    let expanded = quote! {
+        impl #impl_generics CurrentVoltage for #name #ty_generics #where_clause {
+            fn get_current_voltage(&self) -> f32 {
+                self.current_voltage
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
+
+/// Derive macro to automatically implement the `IsSpiking` trait assuming the 
+/// struct has field `is_spiking`
+#[proc_macro_derive(IsSpiking)]
+pub fn derive_is_spiking_trait(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let name = input.ident;
+
+    let generics = &input.generics;
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+    let expanded = quote! {
+        impl #impl_generics IsSpiking for #name #ty_generics #where_clause {
+            fn is_spiking(&self) -> bool {
+                self.is_spiking
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
+
+/// Derive macro to automatically implement the `LastFiringTime` trait assuming the 
+/// struct has field `last_firing_time`
+#[proc_macro_derive(LastFiringTime)]
+pub fn derive_last_firing_time_trait(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let name = input.ident;
+
+    let generics = &input.generics;
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+    let expanded = quote! {
+        impl #impl_generics LastFiringTime for #name #ty_generics #where_clause {
+            fn set_last_firing_time(&mut self, timestep: Option<usize>) {
+                self.last_firing_time = timestep;
+            }
+        
+            fn get_last_firing_time(&self) -> Option<usize> {
+                self.last_firing_time
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
+
+/// Derive macro to automatically implement many necessary traits for the `SpikeTrain` trait,
+/// including `CurrentVoltage`, `LastFiringTime`, and `IsSpiking`
+#[proc_macro_derive(SpikeTrainBase)]
+pub fn derive_spike_train_trait(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let name = input.ident;
+
+    let generics = &input.generics;
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+    let expanded = quote! {
+        impl #impl_generics CurrentVoltage for #name #ty_generics #where_clause {
+            fn get_current_voltage(&self) -> f32 {
+                self.current_voltage
+            }
+        }
+
+        impl #impl_generics IsSpiking for #name #ty_generics #where_clause {
+            fn is_spiking(&self) -> bool {
+                self.is_spiking
+            }
+        }
+
+        impl #impl_generics LastFiringTime for #name #ty_generics #where_clause {
+            fn set_last_firing_time(&mut self, timestep: Option<usize>) {
+                self.last_firing_time = timestep;
+            }
+        
+            fn get_last_firing_time(&self) -> Option<usize> {
+                self.last_firing_time
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
