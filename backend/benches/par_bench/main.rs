8bd4de4617e94bd9f6d17792de3f0c9b3e1e4df1
Nikhiljag <nikhilmukraj@gmail.com>
2024-08-08 17:58:18 -0400

    testing on sparse connections

--- backend/benches/par_bench/main.rs
+++ backend/benches/par_bench/main.rs
@@ -12,6 +12,10 @@ mod tests {
         Lattice,
     };
 
+    fn sparse_connection(x: (usize, usize), y: (usize, usize)) -> bool {
+        (((x.0 as f32 - y.0 as f32).powf(2.) + (x.1 as f32 - y.1 as f32).powf(2.))).sqrt() <= 3. && 
+        x != y
+    }
 
     #[bench]
     fn bench_reg_5x5(b: &mut Bencher) {
@@ -69,6 +73,74 @@ mod tests {
         let mut grid10x10 = Lattice::default_impl();
         grid10x10.populate(&IzhikevichNeuron::default_impl(), 10, 10);
         grid10x10.connect(&|x, y| x != y, None);
+
+        b.iter(|| {
+            grid10x10.apply(
+                |neuron| {
+                    let mut rng = rand::thread_rng();
+                    neuron.current_voltage = rng.gen_range(neuron.v_init..neuron.v_th);
+                }
+            );
+            grid10x10.run_par_inputs_lattice(1).expect("Could not run lattice");
+        })
+    }
+
+    #[bench]
+    fn bench_sparse_reg_5x5(b: &mut Bencher) {
+        let mut grid5x5 = Lattice::default_impl();
+        grid5x5.populate(&IzhikevichNeuron::default_impl(), 5, 5);
+        grid5x5.connect(&sparse_connection, None);
+
+        b.iter(|| {
+            grid5x5.apply(
+                |neuron| {
+                    let mut rng = rand::thread_rng();
+                    neuron.current_voltage = rng.gen_range(neuron.v_init..neuron.v_th);
+                }
+            );
+            grid5x5.run_lattice(1).expect("Could not run lattice");
+        })
+    }
+
+    #[bench]
+    fn bench_sparse_par_5x5(b: &mut Bencher) {
+        let mut grid5x5 = Lattice::default_impl();
+        grid5x5.populate(&IzhikevichNeuron::default_impl(), 5, 5);
+        grid5x5.connect(&sparse_connection, None);
+
+        b.iter(|| {
+            grid5x5.apply(
+                |neuron| {
+                    let mut rng = rand::thread_rng();
+                    neuron.current_voltage = rng.gen_range(neuron.v_init..neuron.v_th);
+                }
+            );
+            grid5x5.run_par_inputs_lattice(1).expect("Could not run lattice");
+        })
+    }
+
+    #[bench]
+    fn bench_sparse_reg_10x10(b: &mut Bencher) {
+        let mut grid10x10 = Lattice::default_impl();
+        grid10x10.populate(&IzhikevichNeuron::default_impl(), 10, 10);
+        grid10x10.connect(&sparse_connection, None);
+
+        b.iter(|| {
+            grid10x10.apply(
+                |neuron| {
+                    let mut rng = rand::thread_rng();
+                    neuron.current_voltage = rng.gen_range(neuron.v_init..neuron.v_th);
+                }
+            );
+            grid10x10.run_lattice(1).expect("Could not run lattice");
+        })
+    }
+
+    #[bench]
+    fn bench_sparse_par_10x10(b: &mut Bencher) {
+        let mut grid10x10 = Lattice::default_impl();
+        grid10x10.populate(&IzhikevichNeuron::default_impl(), 10, 10);
+        grid10x10.connect(&sparse_connection, None);
 
         b.iter(|| {
             grid10x10.apply(
