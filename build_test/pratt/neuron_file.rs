6d9dc3e21fb47f97418732f7184eac3dfb27b3c3
Nikhiljag <nikhilmukraj@gmail.com>
2024-08-09 17:21:14 -0400

    neuron file testing

--- build_test/pratt/neuron_file.rs
+++ build_test/pratt/neuron_file.rs
@@ -6,60 +6,6 @@ use spiking_neural_networks::neuron::ion_channels::TimestepIndependentIonChannel
 
 
 #[derive(Debug, Clone, IterateAndSpikeBase)]
-pub struct BasicIntegrateAndFire<T: NeurotransmitterKinetics, R: ReceptorKinetics> {
-	pub current_voltage: f32,
-	pub e: f32,
-	pub v_reset: f32,
-	pub v_th: f32,
-	pub gap_conductance: f32,
-	pub dt: f32,
-	pub c_m: f32,
-	pub is_spiking: bool,
-	pub last_firing_time: Option<usize>,
-	pub gaussian_params: GaussianParameters,
-	pub synaptic_neurotransmitters: Neurotransmitters<T>,
-	pub ligand_gates: LigandGatedChannels<R>,
-}
-
-impl<T: NeurotransmitterKinetics, R: ReceptorKinetics> BasicIntegrateAndFire<T, R> {
-	fn handle_spiking(&mut self) -> bool {
-		self.is_spiking = self.current_voltage >= self.v_th;
-		if self.is_spiking {
-			self.current_voltage = self.v_reset;
-		}
-	
-		self.is_spiking
-	}
-}
-
-impl<T: NeurotransmitterKinetics, R: ReceptorKinetics> IterateAndSpike for BasicIntegrateAndFire<T, R> {
-	fn get_neurotransmitter_concentrations(&self) -> NeurotransmitterConcentrations {
-		self.synaptic_neurotransmitters.get_concentrations()
-	}
-	
-	fn iterate_and_spike(&mut self, input_current: f32) -> bool {
-		let dv = (((self.current_voltage - self.e) + input_current)) * self.dt;
-		self.current_voltage += dv;
-		self.synaptic_neurotransmitters.apply_t_changes(self.current_voltage);
-		self.handle_spiking()
-	}
-	
-	fn iterate_with_neurotransmitter_and_spike(
-		&mut self,
-		input_current: f32,
-		t_total: &NeurotransmitterConcentrations,
-	) -> bool {
-		self.ligand_gates.update_receptor_kinetics(t_total);
-		self.ligand_gates.set_receptor_currents(self.current_voltage);
-		let dv = (((self.current_voltage - self.e) + input_current)) * self.dt;
-		self.current_voltage += dv;
-		self.current_voltage += self.ligand_gates.get_receptor_currents(self.dt, self.c_m);
-		self.synaptic_neurotransmitters.apply_t_changes(self.current_voltage);
-		self.handle_spiking()
-	}
-}
-
-#[derive(Debug, Clone, IterateAndSpikeBase)]
 pub struct IonChannelNeuron<T: NeurotransmitterKinetics, R: ReceptorKinetics> {
 	pub current_voltage: f32,
 	pub r: f32,
@@ -114,20 +60,58 @@ impl<T: NeurotransmitterKinetics, R: ReceptorKinetics> IterateAndSpike for IonCh
 	}
 }
 
-#[derive(Debug, Clone, Copy)]
-pub struct TestChannel {
+#[derive(Debug, Clone, IterateAndSpikeBase)]
+pub struct BasicIntegrateAndFire<T: NeurotransmitterKinetics, R: ReceptorKinetics> {
+	pub current_voltage: f32,
 	pub e: f32,
-	pub g: f32,
-	pub n: BasicGatingVariable,
-	pub current: f32,
+	pub v_reset: f32,
+	pub v_th: f32,
+	pub gap_conductance: f32,
+	pub dt: f32,
+	pub c_m: f32,
+	pub is_spiking: bool,
+	pub last_firing_time: Option<usize>,
+	pub gaussian_params: GaussianParameters,
+	pub synaptic_neurotransmitters: Neurotransmitters<T>,
+	pub ligand_gates: LigandGatedChannels<R>,
 }
 
-impl TimestepIndependentIonChannel for TestChannel {
-	fn update_current(&mut self, voltage: f32) {
-		self.current = ((self.g * self.n.state) * (self.current_voltage - self.e));
+impl<T: NeurotransmitterKinetics, R: ReceptorKinetics> BasicIntegrateAndFire<T, R> {
+	fn handle_spiking(&mut self) -> bool {
+		self.is_spiking = self.current_voltage >= self.v_th;
+		if self.is_spiking {
+			self.current_voltage = self.v_reset;
+		}
+	
+		self.is_spiking
 	}
+}
 
-	fn get_current(&self) -> f32 { self.current }
+impl<T: NeurotransmitterKinetics, R: ReceptorKinetics> IterateAndSpike for BasicIntegrateAndFire<T, R> {
+	fn get_neurotransmitter_concentrations(&self) -> NeurotransmitterConcentrations {
+		self.synaptic_neurotransmitters.get_concentrations()
+	}
+	
+	fn iterate_and_spike(&mut self, input_current: f32) -> bool {
+		let dv = (((self.current_voltage - self.e) + input_current)) * self.dt;
+		self.current_voltage += dv;
+		self.synaptic_neurotransmitters.apply_t_changes(self.current_voltage);
+		self.handle_spiking()
+	}
+	
+	fn iterate_with_neurotransmitter_and_spike(
+		&mut self,
+		input_current: f32,
+		t_total: &NeurotransmitterConcentrations,
+	) -> bool {
+		self.ligand_gates.update_receptor_kinetics(t_total);
+		self.ligand_gates.set_receptor_currents(self.current_voltage);
+		let dv = (((self.current_voltage - self.e) + input_current)) * self.dt;
+		self.current_voltage += dv;
+		self.current_voltage += self.ligand_gates.get_receptor_currents(self.dt, self.c_m);
+		self.synaptic_neurotransmitters.apply_t_changes(self.current_voltage);
+		self.handle_spiking()
+	}
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -140,6 +124,22 @@ pub struct TestLeak {
 impl TimestepIndependentIonChannel for TestLeak {
 	fn update_current(&mut self, voltage: f32) {
 		self.current = (self.g * (self.current_voltage - self.e));
+	}
+
+	fn get_current(&self) -> f32 { self.current }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct TestChannel {
+	pub e: f32,
+	pub g: f32,
+	pub n: BasicGatingVariable,
+	pub current: f32,
+}
+
+impl TimestepIndependentIonChannel for TestChannel {
+	fn update_current(&mut self, voltage: f32) {
+		self.current = ((self.g * self.n.state) * (self.current_voltage - self.e));
 	}
 
 	fn get_current(&self) -> f32 { self.current }
