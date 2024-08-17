851e5cbef8c15fc0ee031212f368c97983086a0d
Nikhiljag <nikhilmukraj@gmail.com>
2024-08-17 18:05:18 -0400

    new doctest

--- backend/src/neuron/attractors/mod.rs
+++ backend/src/neuron/attractors/mod.rs
@@ -77,6 +77,104 @@
 //!     Ok(())
 //! }
 //! ```
+//! 
+//! Example binary autoassociative network:
+//! ```rust
+//! # use spiking_neural_networks::{
+//! #     neuron::{
+//! #         integrate_and_fire::IzhikevichNeuron,
+//! #         plasticity::STDP,
+//! #         attractors::{
+//! #            generate_random_binary_patterns, generate_binary_hopfield_network, distort_binary_pattern
+//! #         },
+//! #         Lattice, LatticeNetwork, SpikeHistory,
+//! #     },
+//! #     graph::AdjacencyMatrix,
+//! #     error::SpikingNeuralNetworksError,
+//! # };
+//! #
+//! fn main() -> Result<(), SpikingNeuralNetworksError> {
+//!     let (num_rows, num_cols) = (5, 5);
+//!     let base_neuron = IzhikevichNeuron {
+//!         gap_conductance: 5.,
+//!         ..IzhikevichNeuron::default_impl()
+//!     };
+//! 
+//!     let mut inh: Lattice<_, _, SpikeHistory, STDP, _> = Lattice::default();
+//!     inh.populate(&base_neuron, 3, 3);
+//!     inh.connect(&(|x, y| x != y), Some(&(|_, _| -1.5)));
+//! 
+//!     let mut exc: Lattice<_, _, SpikeHistory, STDP, _> = Lattice::default();
+//!     exc.update_grid_history = true;
+//!     exc.populate(&base_neuron, num_rows, num_cols);
+//! 
+//!     // generates random patterns and associated weights
+//!     let random_patterns = generate_random_binary_patterns(num_rows, num_cols, 1, 0.5);
+//!     let binary_connections = generate_binary_hopfield_network::<AdjacencyMatrix<(usize, usize), f32>>(
+//!         1,
+//!         &random_patterns,
+//!         1.,
+//!         1.,
+//!         0.5,
+//!     )?;
+//!     exc.set_graph(binary_connections)?;
+//!     exc.set_id(1);
+//! 
+//!     // initializes lattice with distorted pattern
+//!     let pattern_index = 0;
+//!     let input_pattern = distort_binary_pattern(&random_patterns[pattern_index], 0.1);
+//!     exc.apply_given_position(|pos, neuron| {
+//!         if input_pattern[pos.0][pos.1] == 1 {
+//!             neuron.current_voltage = neuron.v_th;
+//!         } else {
+//!             neuron.current_voltage = neuron.v_init;
+//!         }
+//!     });
+//! 
+//!     let mut network = LatticeNetwork::default_impl();
+//!     network.add_lattice(exc)?;
+//!     network.add_lattice(inh)?;
+//!     network.parallel = true;
+//!     network.connect(
+//!         0, 1, &(|_, _| true), Some(&(|_, _| -1.5))
+//!     );
+//!     network.connect(
+//!         1, 0, &(|_, _| true), Some(&(|_, _| 1.))
+//!     );
+//! 
+//!     network.set_dt(1.);
+//!     network.run_lattices(1000)?;
+//! 
+//!     // associates each firing rate to a low and high state
+//!     let mut firing_rates = network.get_lattice(&1).expect("Could not retrieve lattice")
+//!         .grid_history.aggregate();
+//!     let firing_threshold: isize = 5;
+//!     firing_rates.iter_mut()
+//!         .for_each(|row| {
+//!             row.iter_mut().for_each(|i| {
+//!                 if *i >= firing_threshold {
+//!                     *i = 1;
+//!                 } else {
+//!                     *i = 0;
+//!                 }
+//!             })
+//!         });
+//!     
+//!     // checks accuracy of recall
+//!     let mut accuracy = 0.;
+//!     for (row1, row2) in firing_rates.iter().zip(random_patterns[pattern_index].iter()) {
+//!         for (item1, item2) in row1.iter().zip(row2.iter()) {
+//!             if item1 == item2 {
+//!                 accuracy += 1.;
+//!             }
+//!         }
+//!     }
+//!     
+//!     assert!(accuracy / (num_rows * num_cols) as f32 >= 0.95);
+//! 
+//!     Ok(())
+//! }
+//! ```
 
 use std::result;
 use rand::Rng;
@@ -367,7 +465,115 @@ pub fn generate_hopfield_network<T: Graph<K=(usize, usize), V=f32> + Default>(
     Ok(weights)
 }
 
-/// Adds random noise to a given pattern based on a given `noise_level` between `0.` and `1.`
+fn binary_pattern_calculation(flattened_pattern: &Vec<isize>, a: f32, b: f32, scalar: f32) -> Vec<Vec<f32>> {
+    let mut output: Vec<Vec<f32>> = Vec::new();
+
+    for i in flattened_pattern {
+        let mut vector: Vec<f32> = Vec::new();
+        for j in flattened_pattern {
+            vector.push((*i as f32 - b) * (*j as f32 - a) * scalar);
+        }
+
+        output.push(vector);
+    }
+
+    output
+}
+
+/// Generates weights for a Hopfield network based on a given set of patterns, and 
+/// an id to assign to the graph, assumes the patterns have the same dimensions throughout,
+/// also assumes the pattern is completely binary (either `0` or `1`)
+pub fn generate_binary_hopfield_network<T: Graph<K=(usize, usize), V=f32> + Default>(
+    graph_id: usize,
+    data: &Vec<Vec<Vec<isize>>>,
+    a: f32,
+    b: f32,
+    scalar: f32,
+) -> result::Result<T, SpikingNeuralNetworksError> {
+    let num_rows = data.get(0).unwrap_or(&vec![]).len();
+    let num_cols = data.get(0).unwrap_or(&vec![])
+        .get(0).unwrap_or(&vec![])
+        .len();
+
+    for pattern in data {
+        for row in pattern {
+            if row.iter().any(|i| *i != 1 && *i != 0) {
+                return Err(
+                    SpikingNeuralNetworksError::from(PatternError::PatternIsNotBinary)
+                )
+            }
+        }
+
+        if pattern.len() != num_rows {
+            return Err(
+                SpikingNeuralNetworksError::from(PatternError::PatternDimensionsAreNotEqual)
+            );
+        }
+    
+        if pattern.iter().any(|row| row.len() != num_cols) {
+            return Err(
+                SpikingNeuralNetworksError::from(PatternError::PatternDimensionsAreNotEqual)
+            );
+        }
+    }
+
+    let mut weights = T::default();
+    weights.set_id(graph_id);
+
+    for i in 0..num_rows {
+        for j in 0..num_cols {
+            weights.add_node((i, j));
+        }
+    }
+
+    for pattern in data {
+        let flattened_pattern: Vec<isize> = pattern.iter()
+            .flat_map(|v| v.iter().cloned())
+            .collect();
+
+        let weight_changes = binary_pattern_calculation(
+            &flattened_pattern,
+            a,
+            b,
+            scalar,
+        );
+
+        for (i, weight_vec) in weight_changes.iter().enumerate() {
+            for (j, value) in weight_vec.iter().enumerate() {
+                let coming = first_dimensional_index_to_position(i, num_cols);
+                let going = first_dimensional_index_to_position(j, num_cols);
+
+                //   1 2 3 ...
+                // 1 . . .
+                // 2 . . .
+                // 3 . . .
+                // ...
+                
+                //       (0, 0) (0, 1) (0, 2) ...
+                // (0, 0)   .      .      .
+                // (0, 1)   .      .      .
+                // (0, 2)   .      .      .
+                // ...
+
+                if coming == going {
+                    weights.edit_weight(&coming, &going, None)?;
+                    continue;
+                }
+
+                let current_weight = match weights.lookup_weight(&coming, &going)? {
+                    Some(w) => w,
+                    None => 0.,
+                };
+
+                weights.edit_weight(&coming, &going, Some(current_weight + *value))?;
+            }
+        }  
+    }
+
+    Ok(weights)
+}
+
+/// Adds random noise to a given bipolar pattern based on a given `noise_level` between `0.` and `1.`
 pub fn distort_pattern(pattern: &Vec<Vec<isize>>, noise_level: f32) -> Vec<Vec<isize>> {
     let mut output: Vec<Vec<isize>> = Vec::new();
 
@@ -391,7 +597,31 @@ pub fn distort_pattern(pattern: &Vec<Vec<isize>>, noise_level: f32) -> Vec<Vec<i
     output
 }
 
-/// Generates a random pattern based on a given size, number of patterns
+/// Adds random noise to a given binary pattern based on a given `noise_level` between `0.` and `1.`
+pub fn distort_binary_pattern(pattern: &Vec<Vec<isize>>, noise_level: f32) -> Vec<Vec<isize>> {
+    let mut output: Vec<Vec<isize>> = Vec::new();
+
+    for i in pattern.iter() {
+        let mut output_vec: Vec<isize> = Vec::new();
+        for j in i.iter() {
+            if rand::thread_rng().gen_range(0.0..=1.0) <= noise_level {
+                if *j > 0 {
+                    output_vec.push(0);
+                } else {
+                    output_vec.push(1);
+                }
+            } else {
+                output_vec.push(*j)
+            }
+        }
+
+        output.push(output_vec);
+    }
+
+    output
+}
+
+/// Generates a random bipolar pattern based on a given size, number of patterns
 /// and degree of noise to use when generating the pattern
 pub fn generate_random_patterns(
     num_rows: usize, 
@@ -411,6 +641,30 @@ pub fn generate_random_patterns(
                 } else {
                     1
                 }
+            }).collect();
+
+            current_pattern.chunks(num_cols)
+                .map(|chunk| chunk.to_vec())
+                .collect()
+        })
+        .collect::<Vec<Vec<Vec<isize>>>>()
+}
+
+/// Generates a random binary pattern based on a given size, number of patterns
+/// and degree of noise to use when generating the pattern
+pub fn generate_random_binary_patterns(
+    num_rows: usize, 
+    num_cols: usize, 
+    num_patterns: usize, 
+    p_zero: f32
+) -> Vec<Vec<Vec<isize>>> {
+    let binomial = Binomial::new(1, p_zero.into()).expect("Could not create binomial distribution");
+    let mut rng = rand::thread_rng();
+
+    (0..num_patterns)
+        .map(|_| {
+            let current_pattern: Vec<isize> = binomial.sample_iter(&mut rng).take(num_rows * num_cols).map(|i| {
+                i as isize
             }).collect();
 
             current_pattern.chunks(num_cols)
