545be039f600ed7a8141c39db0336e705910c1ae
Nikhiljag <nikhilmukraj@gmail.com>
2024-09-01 13:40:55 -0400

    gpu execution, need to check results against cpu

--- gpu_impl/opencl/graph_test/src/main.rs
+++ gpu_impl/opencl/graph_test/src/main.rs
@@ -1,27 +1,27 @@
 use opencl3::command_queue::{CommandQueue, CL_QUEUE_PROFILING_ENABLE, CL_QUEUE_SIZE};
 use opencl3::context::Context;
 use opencl3::device::{get_all_devices, Device, CL_DEVICE_TYPE_GPU};
-use opencl3::kernel::Kernel; // ExecuteKernel
-// use opencl3::memory::{Buffer, CL_MEM_READ_ONLY}; // CL_MEM_WRITE_ONLY
+use opencl3::kernel::{Kernel, ExecuteKernel};
+use opencl3::memory::{Buffer, CL_MEM_READ_ONLY}; // CL_MEM_WRITE_ONLY
 use opencl3::program::Program;
-// use opencl3::types::{cl_float, cl_uint, cl_event, CL_BLOCKING, CL_NON_BLOCKING};
+use opencl3::types::{cl_bool, cl_float, cl_uint, cl_event, CL_BLOCKING, CL_NON_BLOCKING};
 use opencl3::Result;
-// use std::ptr;
+use std::ptr;
 use rand::Rng;
 
 
 const PROGRAM_SOURCE: &str = r#"
 __kernel void incoming_connections_sum(
-    __global const uint *connections, 
-    __global_const float *weights, 
+    __global const bool *connections, 
+    __global const float *weights, 
     uint n, 
-    __global float *res,
+    __global float *res
 ) {
     int gid = get_global_id(0);
 
     float sum = 0.0f;
     for (int i = 0; i < n; i++) {
-        if (adjacency_matrix[i * n + gid] != 0) {
+        if (connections[i * n + gid] != 0) {
             sum += weights[i * n + gid];
         }
     }
@@ -32,18 +32,18 @@ __kernel void incoming_connections_sum(
 
 const KERNEL_NAME: &str = "incoming_connections_sum";
 
-fn create_random_flattened_adj_matrix(size: u32, lower_bound: f32, upper_bound: f32) -> (Vec<u32>, Vec<f32>) {
-    let mut connections = Vec::new();
-    let mut weights = Vec::new();
+fn create_random_flattened_adj_matrix(size: usize, lower_bound: f32, upper_bound: f32) -> (Vec<bool>, Vec<f32>) {
+    let full_size = size * size;
+    
+    let mut connections = vec![false; full_size];
+    let mut weights = vec![0.; full_size];
 
     let mut rng = rand::thread_rng();
 
-    for i in 0..(size * size) {
-        connections[i as usize] = rng.gen_range(0..=1);
-        if connections[i as usize] != 0 {
-            weights[i as usize] = rng.gen_range(lower_bound..=upper_bound);
-        } else {
-            weights[i as usize] = 0.;
+    for i in 0..(full_size) {
+        connections[i] = rng.gen_bool(0.5);
+        if connections[i] {
+            weights[i] = rng.gen_range(lower_bound..=upper_bound);
         }
     }
 
@@ -67,7 +67,7 @@ fn main() -> Result<()> {
 
     let context = Context::from_device(&device).expect("Context::from_device failed");
 
-    let _queue = CommandQueue::create_default_with_properties(
+    let queue = CommandQueue::create_default_with_properties(
             &context, 
             CL_QUEUE_PROFILING_ENABLE,
             CL_QUEUE_SIZE,
@@ -77,24 +77,69 @@ fn main() -> Result<()> {
     // build kernel
     let program = Program::create_and_build_from_source(&context, PROGRAM_SOURCE, "")
         .expect("Program::create_and_build_from_source failed");
-    let _kernel = Kernel::create(&program, KERNEL_NAME).expect("Kernel::create failed");
+    let kernel = Kernel::create(&program, KERNEL_NAME).expect("Kernel::create failed");
 
     // number of nodes
-    const N: u32 = 10;
+    const N: usize = 100;
 
-    let (_connections, _weights) = create_random_flattened_adj_matrix(N, 0., 2.);
+    let (connections, weights) = create_random_flattened_adj_matrix(N, 0., 2.);
+    let mut connections_array: [cl_bool; N * N] = [0; N * N];
+    let mut weights_array: [cl_float; N * N] = [0.; N * N];
+    let sums_array: [cl_float; N] = [0.; N];
+
+    for i in 0..(N * N) {
+        connections_array[i] = connections[i] as u32;
+        weights_array[i] = weights[i];
+    }
+
+    let mut connections_buffer = unsafe {
+        Buffer::<cl_bool>::create(&context, CL_MEM_READ_ONLY, N * N, ptr::null_mut())?
+    };
+    let mut weights_buffer = unsafe {
+        Buffer::<cl_float>::create(&context, CL_MEM_READ_ONLY, N * N, ptr::null_mut())?
+    };
+    let mut sums_buffer = unsafe {
+        Buffer::<cl_float>::create(&context, CL_MEM_READ_ONLY, N, ptr::null_mut())?
+    };
+
+    let _connections_write_event = unsafe { 
+        queue.enqueue_write_buffer(&mut connections_buffer, CL_BLOCKING, 0, &connections_array, &[])? 
+    };
+    let _weights_write_event = unsafe { 
+        queue.enqueue_write_buffer(&mut weights_buffer, CL_BLOCKING, 0, &weights_array, &[])? 
+    };
+    let sums_write_event = unsafe { 
+        queue.enqueue_write_buffer(&mut sums_buffer, CL_NON_BLOCKING, 0, &sums_array, &[])? 
+    };
+
+    let n_cl: cl_uint = N as u32;
 
     // execute kernel
-    // let kernel_event = unsafe {
-    //     ExecuteKernel::new(&kernel)
-    //         .set_arg(&matrix_buffer)
-    //         .set_arg(&sums_buffer)
-    //         .set_arg(&m_cl)
-    //         .set_arg(&n_cl)
-    //         .set_global_work_size(N)
-    //         .set_wait_event(&sums_write_event)
-    //         .enqueue_nd_range(&queue)?
-    // };
+    let kernel_event = unsafe {
+        ExecuteKernel::new(&kernel)
+            .set_arg(&connections_buffer)
+            .set_arg(&weights_buffer)
+            .set_arg(&n_cl)
+            .set_arg(&sums_buffer)
+            .set_global_work_size(N) // number of threads executing in parallel
+            .set_wait_event(&sums_write_event)
+            .enqueue_nd_range(&queue)?
+    };
+
+    let events: Vec<cl_event> = vec![kernel_event.get()];
+
+    let mut results: [cl_float; N] = [0.0; N];
+    let results_read_event = unsafe {
+        queue.enqueue_read_buffer(&sums_buffer, CL_NON_BLOCKING, 0, &mut results, &events)?
+    };
+
+    results_read_event.wait()?;
+    
+    let start_time = kernel_event.profiling_command_start()?;
+    let end_time = kernel_event.profiling_command_end()?;
+    let duration = end_time - start_time;
+
+    println!("Graph execution time (ns): {}", duration);
 
     Ok(())
 }
