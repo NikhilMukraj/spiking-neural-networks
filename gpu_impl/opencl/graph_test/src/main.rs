a470680ce1c218b0085b7f38afca3ce91331d08c
Nikhiljag <nikhilmukraj@gmail.com>
2024-09-02 19:32:39 -0400

    fixed stack overflow error with larger values for N

--- gpu_impl/opencl/graph_test/src/main.rs
+++ gpu_impl/opencl/graph_test/src/main.rs
@@ -11,6 +11,15 @@ use std::time::Instant;
 use rand::Rng;
 
 
+// take in voltage and gap conductance as arrays of size N
+// presynaptic voltage should be i
+// postsynaptic voltage should be gid
+// gap conductance should be from postsynaptic neuron
+// weight * (gap_conductance * (v_pre - v_post))
+// count how many connections
+// if connection count is 0, return 0
+// otherwise average input
+
 const PROGRAM_SOURCE: &str = r#"
 __kernel void incoming_connections_sum(
     __global const uint *connections, 
@@ -32,6 +41,37 @@ __kernel void incoming_connections_sum(
 "#;
 
 const KERNEL_NAME: &str = "incoming_connections_sum";
+
+// const PROGRAM_SOURCE: &str = r#"
+// __kernel void calculate_internal_electrical_inputs(
+//     __global const uint *connections, 
+//     __global const float *weights, 
+//     __global const float *gap_conductances,
+//     __global const float *voltages,
+//     uint n, 
+//     __global float *res
+// ) {
+//     int gid = get_global_id(0);
+
+//     float sum = 0.0f;
+//     int count = 0;
+//     for (int i = 0; i < n; i++) {
+//         if (connections[i * n + gid] == 1) {
+//             float gap_junction = gap_conductances[gid] * (voltages[i] - voltages[gid]);
+//             sum += weights[i * n + gid] * gap_junction;
+//             count++;
+//         }
+//     }
+    
+//     if (count != 0) {
+//         res[gid] = sum / count;
+//     } else {
+//         res[gif] = 0;
+//     }
+// }
+// "#;
+
+// const KERNEL_NAME: &str = "calculate_internal_electrical_inputs";
 
 fn create_random_flattened_adj_matrix(size: usize, lower_bound: f32, upper_bound: f32) -> (Vec<bool>, Vec<f32>) {
     let full_size = size * size;
@@ -97,12 +137,21 @@ fn main() -> Result<()> {
     // weight matrix (value of weight if connecting)
     // calculate gap junction (insert gap junction kernel at runtime if possible)
 
+    // iterate and spike with leaky integrate and fire
+    // benchmark the time taken to execute the kernel multiple times
+    // make sure moves between cpu and gpu are minimal
+
     // array where index corresponds to a position in cell grid
     // index in graph can that be associated with cell grid positions
     // assume non-ragged for now
 
-    // note that using cl_bool resulted in incorrect computation
-    // switching to cl_uint fixed this
+    // adapt iterate and spike to new indexing system
+    
+    // simple voltage history tracking and and average voltage history and spike history tracking
+    // rolling firing rate, array size equal to number of neurons, array is incremented over time
+    // if a spike occurs
+    
+    // multiple executions of gpu kernels over time
 
     let device_id = *get_all_devices(CL_DEVICE_TYPE_GPU)?
         .first()
@@ -124,7 +173,7 @@ fn main() -> Result<()> {
     let kernel = Kernel::create(&program, KERNEL_NAME).expect("Kernel::create failed");
 
     // number of nodes
-    const N: usize = 100;
+    const N: usize = 1000;
 
     let (connections, weights) = create_random_flattened_adj_matrix(N, 0., 2.);
 
@@ -132,9 +181,9 @@ fn main() -> Result<()> {
     let cpu_results = cpu_incoming_connections_sum(&connections, &weights, N);
     let cpu_duration = start.elapsed().as_nanos();
 
-    let mut connections_array: [cl_uint; N * N] = [0; N * N];
-    let mut weights_array: [cl_float; N * N] = [0.; N * N];
-    let sums_array: [cl_float; N] = [0.; N];
+    let mut connections_array: Vec<cl_uint> = vec![0; N * N];
+    let mut weights_array: Vec<cl_float> = vec![0.; N * N];
+    let sums_array: Vec<cl_float> = vec![0.; N];
 
     for i in 0..(N * N) {
         connections_array[i] = connections[i] as u32;
