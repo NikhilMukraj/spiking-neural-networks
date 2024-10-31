68a16b5b2136c352ae3e72e5ce72fd097f58cbd4
Nikhiljag <nikhilmukraj@gmail.com>
2024-10-30 20:47:42 -0400

    new neurotransmitters test

--- backend/tests/neurotransmitter_conversion.rs
+++ backend/tests/neurotransmitter_conversion.rs
@@ -12,6 +12,56 @@ mod tests {
     };
     use spiking_neural_networks::error::SpikingNeuralNetworksError;
 
+
+    #[test]
+    pub fn test_empty_neurotransmitter_conversion() -> Result<(), SpikingNeuralNetworksError> {
+        let device_id = *get_all_devices(CL_DEVICE_TYPE_GPU)
+            .expect("Could not get GPU devices")
+            .first()
+            .expect("No GPU found");
+        let device = Device::new(device_id);
+
+        let context = Context::from_device(&device).expect("Context::from_device failed");
+
+        let queue = CommandQueue::create_default_with_properties(
+                &context, 
+                CL_QUEUE_PROFILING_ENABLE,
+                CL_QUEUE_SIZE,
+            )
+            .expect("CommandQueue::create_default failed");
+
+        type GridType = Vec<Vec<Neurotransmitters<IonotropicNeurotransmitterType, ApproximateNeurotransmitter>>>;
+        let neurotransmitters_grid: GridType = vec![];
+
+        let gpu_conversion = Neurotransmitters::convert_to_gpu(
+            &neurotransmitters_grid,
+            &context,
+            &queue,
+            1,
+            4,
+        )?;
+
+        let mut cpu_conversion = neurotransmitters_grid.clone();
+        Neurotransmitters::convert_to_cpu(
+            &mut cpu_conversion,
+            &gpu_conversion,
+            &queue,
+            1,
+            4,
+        )?;
+
+        for (row1, row2) in cpu_conversion.iter().zip(neurotransmitters_grid.iter()) {
+            for (actual, expected) in row1.iter().zip(row2.iter()) {
+                assert_eq!(
+                    actual, 
+                    expected,
+                );
+            }
+        }
+
+        Ok(())
+    }
+
     #[test]
     pub fn test_neurotransmitter_conversion() -> Result<(), SpikingNeuralNetworksError> {
         let mut neurotransmitters1 = Neurotransmitters::default();
