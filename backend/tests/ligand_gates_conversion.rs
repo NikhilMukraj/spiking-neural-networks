decbe67caa99e2f2e8dbf2600638fd7d8b57304a
Nikhiljag <nikhilmukraj@gmail.com>
2024-11-01 18:37:57 -0400

    ligand gates conversion tests pass

--- backend/tests/ligand_gates_conversion.rs
+++ backend/tests/ligand_gates_conversion.rs
@@ -13,6 +13,55 @@ mod tests {
     use spiking_neural_networks::error::SpikingNeuralNetworksError;
 
     #[test]
+    pub fn test_empty_ligand_gates_conversion() -> Result<(), SpikingNeuralNetworksError> {
+        let device_id = *get_all_devices(CL_DEVICE_TYPE_GPU)
+            .expect("Could not get GPU devices")
+            .first()
+            .expect("No GPU found");
+        let device = Device::new(device_id);
+
+        let context = Context::from_device(&device).expect("Context::from_device failed");
+
+        let queue = CommandQueue::create_default_with_properties(
+                &context, 
+                CL_QUEUE_PROFILING_ENABLE,
+                CL_QUEUE_SIZE,
+            )
+            .expect("CommandQueue::create_default failed");
+
+        type GridType = Vec<Vec<LigandGatedChannels<ApproximateReceptor>>>;
+        let ligand_gates_grid: GridType = vec![];
+
+        let gpu_conversion = LigandGatedChannels::convert_to_gpu(
+            &ligand_gates_grid,
+            &context,
+            &queue,
+            0,
+            0,
+        )?;
+
+        let mut cpu_conversion = ligand_gates_grid.clone();
+        LigandGatedChannels::convert_to_cpu(
+            &mut cpu_conversion,
+            &gpu_conversion,
+            &queue,
+            0,
+            0,
+        )?;
+
+        for (row1, row2) in cpu_conversion.iter().zip(ligand_gates_grid.iter()) {
+            for (actual, expected) in row1.iter().zip(row2.iter()) {
+                assert_eq!(
+                    actual, 
+                    expected,
+                );
+            }
+        }
+
+        Ok(())
+    }
+
+    #[test]
     pub fn test_ligand_gates_conversion() -> Result<(), SpikingNeuralNetworksError> {
         let mut ligand_gates1 = LigandGatedChannels::<ApproximateReceptor>::default();
         ligand_gates1.insert(
