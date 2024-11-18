29cea6654187b2f7e7a5baded5030db9942948e4
Nikhiljag <nikhilmukraj@gmail.com>
2024-11-18 16:39:41 -0500

    testing kernel generation

---
+++ backend/tests/checking_program_source.rs
@@ -0,0 +1,34 @@
+mod tests {
+    use opencl3::{
+        // command_queue::{CommandQueue, CL_QUEUE_PROFILING_ENABLE, CL_QUEUE_SIZE},
+        context::Context, device::{get_all_devices, Device, CL_DEVICE_TYPE_GPU},
+    };
+    extern crate spiking_neural_networks;
+    use spiking_neural_networks::{
+        // error::SpikingNeuralNetworksError, 
+        neuron::{
+            integrate_and_fire::QuadraticIntegrateAndFireNeuron, 
+            iterate_and_spike::{
+                ApproximateNeurotransmitter, ApproximateReceptor, IterateAndSpikeGPU,
+            }
+        }
+    };
+
+    #[test]
+    pub fn test_program_source() {
+        let device_id = *get_all_devices(CL_DEVICE_TYPE_GPU)
+            .expect("Could not get GPU devices")
+            .first()
+            .expect("No GPU found");
+        let device = Device::new(device_id);
+
+        let context = Context::from_device(&device).expect("Context::from_device failed");
+
+        let program_source = QuadraticIntegrateAndFireNeuron::<ApproximateNeurotransmitter, ApproximateReceptor>::
+            iterate_and_spike_electrochemical_kernel(&context)
+            .unwrap()
+            .program_source;
+
+        println!("{}", program_source);
+    }
+}
