746af68382a30a0132479e8c0bb495f12ed52cfd
Nikhiljag <nikhilmukraj@gmail.com>
2024-12-03 19:08:12 -0500

    more work on kernel verification tests

--- backend/tests/checking_program_source.rs
+++ backend/tests/checking_program_source.rs
@@ -4,15 +4,12 @@ mod tests {
         context::Context, device::{get_all_devices, Device, CL_DEVICE_TYPE_GPU},
     };
     extern crate spiking_neural_networks;
-    use spiking_neural_networks::{
-        // error::SpikingNeuralNetworksError, 
-        neuron::{
+    use spiking_neural_networks::neuron::{
             integrate_and_fire::QuadraticIntegrateAndFireNeuron, 
             iterate_and_spike::{
-                ApproximateNeurotransmitter, ApproximateReceptor, IterateAndSpikeGPU,
+                AMPADefault, ApproximateNeurotransmitter, ApproximateReceptor, IonotropicNeurotransmitterType, IterateAndSpike, IterateAndSpikeGPU, LigandGatedChannel, NeurotransmitterConcentrations, Timestep
             }
-        }
-    };
+        };
 
     #[test]
     pub fn test_program_source() {
@@ -35,5 +32,32 @@ mod tests {
     //     // initialize 1x1 grid
     //     // give constant ampa input, then constant nmda, gaba, etc
     //     // check against cpu equavilent
+
+    //     // let iterations = 1000;
+        
+    //     // let neuron = QuadraticIntegrateAndFireNeuron::default_impl();
+    //     // neuron.ligand_gates.insert(IonotropicNeurotransmitterType::AMPA, LigandGatedChannel::ampa_default());
+    //     // neuron.ligand_gates.insert(IonotropicNeurotransmitterType::NMDA, LigandGatedChannel::nmda_default());
+    //     // neuron.ligand_gates.insert(IonotropicNeurotransmitterType::GABAa, LigandGatedChannel::gabaa_default());
+
+    //     // neuron.set_dt(1.);
+
+    //     // let cpu_neuron = neuron.clone();
+
+    //     // let cell_grid = vec![vec![neuron]];
+
+    //     // let mut ampa_conc = NeurotransmitterConcentrations::new();
+    //     // ampa_conc.insert(IonotropicNeurotransmitterType::AMPA, 1.0);
+
+    //     // for _ in 0..iterations {
+    //     //     cpu_neuron.iterate_with_neurotransmitter_and_spike(
+    //     //         0, 
+    //     //         &ampa_conc
+    //     //     );
+    //     // }
+
+    //     // create 1 length grid for voltage input, init to 0
+    //     // create N::number_of_types() length grid, init first index to 1
+    //     // should expect only ampa to activate
     // }
 }
