ae99b26547a26d6d673828ab6be05f76da551a9c
Nikhiljag <nikhilmukraj@gmail.com>
2024-12-17 17:16:15 -0500

    spectral radius calculation

--- interface/experiments/isolated_liquid_pipeline.py
+++ interface/experiments/isolated_liquid_pipeline.py
@@ -1,3 +1,9 @@
 # generate liquid with random weights, one exc group and one inh group
 # vary glu, gaba conductance and clearance
 # stdp with mnist input potentially
+
+# import numpy as np
+
+# def spectral_radius(A):
+#     eigenvalues = np.linalg.eigvals(A)
+#     return np.max(np.abs(eigenvalues))
