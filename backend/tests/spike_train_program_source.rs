f398a06e738a27157dab45c81b43d00d2f032384
Nikhiljag <nikhilmukraj@gmail.com>
2024-12-20 14:00:28 -0500

    fixes to tests

--- backend/tests/spike_train_program_source.rs
+++ backend/tests/spike_train_program_source.rs
@@ -89,6 +89,8 @@ mod tests {
 
         neuron.set_dt(1.);
 
+        neuron.chance_of_firing = 0.05;
+
         let cell_grid = vec![vec![neuron]];
 
         let device_id = *get_all_devices(CL_DEVICE_TYPE_GPU)
@@ -202,20 +204,136 @@ mod tests {
         Ok(())
     }
 
+    fn get_gpu_is_spiking(gpu_cell_grid: &HashMap<String, BufferGPU>, queue: &CommandQueue, gpu_tracker: &mut Vec<f32>) -> Result<(), SpikingNeuralNetworksError> {
+        match gpu_cell_grid.get("is_spiking").unwrap() {
+            BufferGPU::UInt(buffer) => {
+                let mut read_vector: Vec<u32> = vec![0];
+
+                let read_event = unsafe {
+                    match queue.enqueue_read_buffer(buffer, CL_NON_BLOCKING, 0, &mut read_vector, &[]) {
+                        Ok(value) => value,
+                        Err(_) => return Err(SpikingNeuralNetworksError::from(GPUError::BufferReadError)),
+                    }
+                };
+    
+                match read_event.wait() {
+                    Ok(value) => value,
+                    Err(_) => return Err(SpikingNeuralNetworksError::from(GPUError::WaitError)),
+                };
+
+                gpu_tracker.push(read_vector[0] as f32);
+            },
+            _ => unreachable!(),
+        }
+
+        Ok(())
+    }
+
     // check if spikes are being emitted
     // check that neurotransmitter is being updated appropriately
 
     #[test]
     pub fn test_voltage() -> Result<(), SpikingNeuralNetworksError> {
-        iterate_neuron(&get_gpu_voltage, true)?;
+        let voltages = iterate_neuron(&get_gpu_voltage, true)?;
+
+        let mut unique_values = vec![];
+
+        for i in voltages {
+            if !unique_values.contains(&i) {
+                unique_values.push(i);
+            }
+        }
+
+        assert_eq!(2, unique_values.len());
+
+        Ok(())
+    }
+
+    #[test]
+    pub fn test_is_spiking() -> Result<(), SpikingNeuralNetworksError> {
+        let is_spikings = iterate_neuron(&get_gpu_is_spiking, true)?;
+
+        let mut unique_values = vec![];
+
+        for i in is_spikings {
+            if !unique_values.contains(&i) {
+                unique_values.push(i);
+            }
+        }
+        
+        assert_eq!(2, unique_values.len());
 
         Ok(())
     }
 
     #[test]
     pub fn test_voltage_when_chemical() -> Result<(), SpikingNeuralNetworksError> {
-        iterate_neuron(&get_gpu_voltage, false)?;
+        let voltages = iterate_neuron(&get_gpu_voltage, false)?;
+
+        let mut unique_values = vec![];
+
+        for i in voltages {
+            if !unique_values.contains(&i) {
+                unique_values.push(i);
+            }
+        }
+
+        assert_eq!(2, unique_values.len());
 
         Ok(())
     }
+
+    #[test]
+    pub fn test_is_spiking_when_chemical() -> Result<(), SpikingNeuralNetworksError> {
+        let is_spikings = iterate_neuron(&get_gpu_is_spiking, false)?;
+
+        let mut unique_values = vec![];
+
+        for i in is_spikings {
+            if !unique_values.contains(&i) {
+                unique_values.push(i);
+            }
+        }
+        
+        assert_eq!(2, unique_values.len());
+
+        Ok(())
+    }
+
+    fn gpu_get_ampa_neurotransmitter(gpu_cell_grid: &HashMap<String, BufferGPU>, queue: &CommandQueue, gpu_tracker: &mut Vec<f32>) -> Result<(), SpikingNeuralNetworksError> {
+        match gpu_cell_grid.get("neurotransmitters$t").unwrap() {
+            BufferGPU::Float(buffer) => {
+                let mut read_vector = vec![0., 0., 0., 0.];
+
+                let read_event = unsafe {
+                    match queue.enqueue_read_buffer(buffer, CL_NON_BLOCKING, 0, &mut read_vector, &[]) {
+                        Ok(value) => value,
+                        Err(_) => return Err(SpikingNeuralNetworksError::from(GPUError::BufferReadError)),
+                    }
+                };
+    
+                match read_event.wait() {
+                    Ok(value) => value,
+                    Err(_) => return Err(SpikingNeuralNetworksError::from(GPUError::WaitError)),
+                };
+
+                gpu_tracker.push(read_vector[0]);
+            },
+            _ => unreachable!(),
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    pub fn test_ampa() -> Result<(), SpikingNeuralNetworksError> {
+        let ampas = iterate_neuron(&gpu_get_ampa_neurotransmitter, false)?;
+
+        for i in ampas {
+            assert!(i <= 1.);
+            assert!(i >= 0.);
+        }
+
+        Ok(())
+    }    
 }
