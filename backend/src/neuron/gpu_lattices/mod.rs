d9abca2198358e57ba8ec1a9718105b6c47def97
Nikhiljag <nikhilmukraj@gmail.com>
2024-12-22 14:11:12 -0500

    start of lattice network implementation

--- backend/src/neuron/gpu_lattices/mod.rs
+++ backend/src/neuron/gpu_lattices/mod.rs
@@ -5,16 +5,20 @@ use opencl3::{
     kernel::{ExecuteKernel, Kernel}, memory::{Buffer, CL_MEM_READ_WRITE}, 
     program::Program, types::{cl_float, CL_NON_BLOCKING},
 };
-use crate::{error::{GPUError, SpikingNeuralNetworksError}, graph::{Graph, GraphGPU, GraphToGPU}};
+use crate::{
+    error::{GPUError, SpikingNeuralNetworksError}, 
+    graph::{ConnectingGraphGPU, Graph, GraphGPU, GraphToGPU, ConnectingGraphToGPU, GraphPosition}
+};
 use super::{
     iterate_and_spike::{
         BufferGPU, IterateAndSpike, IterateAndSpikeGPU, 
-        KernelFunction, NeurotransmitterTypeGPU
+        KernelFunction, NeurotransmitterTypeGPU,
     }, 
-    GridVoltageHistory, RunLattice,
+    spike_train::SpikeTrainGPU,
+    plasticity::Plasticity,
+    GridVoltageHistory, RunLattice, RunNetwork, SpikeTrainLatticeHistory,
+    Lattice, LatticeHistory, Position, LatticeNetwork, SpikeTrainLattice, impl_apply
 };
-use super::plasticity::Plasticity;
-use super::{Lattice, LatticeHistory, Position, impl_apply};
 use std::ptr;
 
 
@@ -302,7 +306,6 @@ pub struct LatticeGPU<
     pub cell_grid: Vec<Vec<T>>,
     graph: U,
     electrical_incoming_connections_kernel: Kernel,
-    #[allow(dead_code)]
     chemical_incoming_connections_kernel: Kernel,
     last_firing_time_kernel: Kernel,
     context: Context,
@@ -778,6 +781,211 @@ where
             (true, false) => self.run_lattice_electrical_synapses(iterations).map_err(Into::into),
             (false, true) => self.run_lattice_chemical_synapses(iterations).map_err(Into::into),
             (true, true) => self.run_lattice_chemical_synapses(iterations).map_err(Into::into),
+            (false, false) => Ok(()),
+        }
+    }
+}
+
+/// An implementation of a lattice network that is compatible with the GPU
+#[allow(dead_code)]
+pub struct LatticeNetworkGPU<
+    T: IterateAndSpike<N=N> + IterateAndSpikeGPU, 
+    U: Graph<K=(usize, usize), V=f32> + GraphToGPU<GraphGPU>, 
+    V: LatticeHistory + LatticeHistoryGPU,
+    W: SpikeTrainGPU<N=N>,
+    X: SpikeTrainLatticeHistory,
+    Y: Plasticity<W, T, f32> + Plasticity<T, T, f32>,
+    N: NeurotransmitterTypeGPU,
+    C: Graph<K=GraphPosition, V=f32> + ConnectingGraphToGPU<ConnectingGraphGPU>, 
+> {
+    lattices: HashMap<usize, Lattice<T, U, V, Y, N>>,
+    spike_train_lattices: HashMap<usize, SpikeTrainLattice<N, W, X>>,
+    connecting_graph: C,
+    electrical_incoming_connections_kernel: Kernel,
+    chemical_incoming_connections_kernel: Kernel,
+    last_firing_time_kernel: Kernel,
+    context: Context,
+    queue: CommandQueue,
+    grid_history_kernels: HashMap<usize, KernelFunction>,
+    pub electrical_synapse: bool,
+    pub chemical_synapse: bool,
+    internal_clock: usize,
+}
+
+impl<T, U, V, W, X, Y, N, C> LatticeNetworkGPU<T, U, V, W, X, Y, N, C>
+where
+    T: IterateAndSpike<N=N> + IterateAndSpikeGPU, 
+    U: Graph<K=(usize, usize), V=f32> + GraphToGPU<GraphGPU>, 
+    V: LatticeHistory + LatticeHistoryGPU,
+    W: SpikeTrainGPU<N=N>,
+    X: SpikeTrainLatticeHistory,
+    Y: Plasticity<W, T, f32> + Plasticity<T, T, f32>,
+    N: NeurotransmitterTypeGPU,
+    C: Graph<K=GraphPosition, V=f32> + ConnectingGraphToGPU<ConnectingGraphGPU>, 
+{
+    // Generates a GPU lattice network given a network and a device
+    pub fn from_network_given_device(
+        lattice_network: LatticeNetwork<T, U, V, W, X, C, Y, N>,
+        device: &Device
+    ) -> Result<Self, GPUError> {
+        let context = match Context::from_device(device) {
+            Ok(value) => value,
+            Err(_) => return Err(GPUError::GetDeviceFailure),
+        };
+
+        let queue =  match CommandQueue::create_default_with_properties(
+            &context, 
+            CL_QUEUE_PROFILING_ENABLE,
+            CL_QUEUE_SIZE,
+        ) {
+            Ok(value) => value,
+            Err(_) => return Err(GPUError::GetDeviceFailure),
+        };
+
+        // let electrical_incoming_connections_program = match Program::create_and_build_from_source(&context, INPUTS_KERNEL, ""){
+        //     Ok(value) => value,
+        //     Err(_) => return Err(GPUError::ProgramCompileFailure),
+        // };
+        // let electrical_incoming_connections_kernel = match Kernel::create(&electrical_incoming_connections_program, INPUTS_KERNEL_NAME) {
+        //     Ok(value) => value,
+        //     Err(_) => return Err(GPUError::KernelCompileFailure),
+        // };
+
+        // let chemical_incoming_connections_program = match Program::create_and_build_from_source(&context, NEUROTRANSMITTER_INPUTS_KERNEL, ""){
+        //     Ok(value) => value,
+        //     Err(_) => return Err(GPUError::ProgramCompileFailure),
+        // };
+        // let chemical_incoming_connections_kernel = match Kernel::create(&chemical_incoming_connections_program, NEUROTRANSMITTER_INPUTS_KERNEL_NAME) {
+        //     Ok(value) => value,
+        //     Err(_) => return Err(GPUError::KernelCompileFailure),
+        // };
+
+        let last_firing_time_program = match Program::create_and_build_from_source(&context, LAST_FIRING_TIME_KERNEL, "") {
+            Ok(value) => value,
+            Err(_) => return Err(GPUError::ProgramCompileFailure),
+        };
+        let last_firing_time_kernel = match Kernel::create(&last_firing_time_program, LAST_FIRING_TIME_KERNEL_NAME) {
+            Ok(value) => value,
+            Err(_) => return Err(GPUError::KernelCompileFailure),
+        };
+
+        Ok(
+            LatticeNetworkGPU { 
+                lattices: lattice_network.get_lattices().clone(), 
+                spike_train_lattices: lattice_network.get_spike_train_lattices().clone(),
+                connecting_graph: lattice_network.get_connecting_graph().clone(), 
+                context, 
+                queue, 
+                last_firing_time_kernel, 
+                electrical_incoming_connections_kernel: todo!(), 
+                #[allow(unreachable_code)]
+                chemical_incoming_connections_kernel: todo!(), 
+                #[allow(unreachable_code)]
+                grid_history_kernels: todo!(),
+                electrical_synapse: lattice_network.electrical_synapse, 
+                chemical_synapse: lattice_network.chemical_synapse, 
+                internal_clock: lattice_network.internal_clock
+            }
+        )
+    }
+
+    // Generates a GPU lattice network from a given lattice network
+    pub fn from_network(
+        lattice_network: LatticeNetwork<T, U, V, W, X, C, Y, N>,
+    ) -> Result<Self, GPUError> {
+        let device_id = *get_all_devices(CL_DEVICE_TYPE_GPU)
+            .expect("Could not get GPU devices")
+            .first()
+            .expect("No GPU found");
+        let device = Device::new(device_id);
+
+        Self::from_network_given_device(lattice_network, &device)
+    }
+
+    fn run_lattices_with_electrical_synapses(&mut self, _iterations: usize) -> Result<(), GPUError> {
+        // concat cell grids into a single 1d vector of cell grids
+        // create a new vector that keeps track of which lattices and positions each cell belongs to
+        // use those vectors to write the new values back to the cpu and execute the connections kernel
+        // connections kernel should take each weight matrix in and use lattice size to index where to go
+
+        let mut cell_vector: Vec<T> = vec![];
+        let mut lattice_ids: Vec<u32> = vec![];
+        let mut lattice_sizes: Vec<u32> = vec![];
+        let mut lattice_sizes_map: HashMap<usize, (usize, usize)> = HashMap::new();
+
+        for (key, value) in &self.lattices {
+            let current_cell_grid = value.get_cell_grid();
+            for row in current_cell_grid {
+                for i in row {
+                    cell_vector.push(i.clone());
+                    lattice_ids.push(*key as u32);
+                }
+            }
+            
+            let rows = current_cell_grid.len();
+            let cols = current_cell_grid.first().unwrap_or(&vec![]).len();
+            lattice_sizes.push((rows * cols) as u32);
+            lattice_sizes_map.insert(*key, (rows, cols));
+        }
+
+        let mut cell_vector = vec![cell_vector];
+        let cell_vector_size = cell_vector.first().unwrap_or(&vec![]).len();
+
+        let gpu_cell_grid = T::convert_to_gpu(&cell_vector, &self.context, &self.queue)?;
+
+        T::convert_electrochemical_to_cpu(
+            &mut cell_vector, 
+            &gpu_cell_grid, 
+            1, 
+            cell_vector_size, 
+            &self.queue
+        )?;
+
+        let mut new_grids: HashMap<usize, Vec<T>> = HashMap::new();
+
+        if let Some(first_vec) = cell_vector.first() {
+            for (id, cell) in lattice_ids.iter().zip(first_vec.iter()) {
+                new_grids.entry(*id as usize)
+                    .and_modify(|vec| vec.push(cell.clone()))
+                    .or_insert_with(|| vec![cell.clone()]);
+            }
+        } // maybe use std::mem::take ?
+
+        let mut reshaped_grids = HashMap::new();
+
+        for (key, vec) in new_grids {
+            if let Some(&(_, cols)) = lattice_sizes_map.get(&key) {
+                let reshaped_vec: Vec<Vec<T>> = vec.chunks(cols)
+                    .map(|chunk| chunk.to_vec())
+                    .collect();
+                reshaped_grids.insert(key, reshaped_vec);
+            }
+        }
+
+        for (key, value) in self.lattices.iter_mut() {
+            value.set_cell_grid(reshaped_grids.get(key).unwrap().clone()).expect("Same dimensions");
+        }
+
+        Ok(())
+    }
+}
+
+impl<T, U, V, W, X, Y, N, C> RunNetwork for LatticeNetworkGPU<T, U, V, W, X, Y, N, C>
+where
+    T: IterateAndSpike<N=N> + IterateAndSpikeGPU, 
+    U: Graph<K=(usize, usize), V=f32> + GraphToGPU<GraphGPU>, 
+    V: LatticeHistory + LatticeHistoryGPU,
+    W: SpikeTrainGPU<N=N>,
+    X: SpikeTrainLatticeHistory,
+    Y: Plasticity<W, T, f32> + Plasticity<T, T, f32>,
+    N: NeurotransmitterTypeGPU,
+    C: Graph<K=GraphPosition, V=f32> + ConnectingGraphToGPU<ConnectingGraphGPU>, 
+{
+    fn run_lattices(&mut self, iterations: usize) -> Result<(), SpikingNeuralNetworksError> {
+        match (self.electrical_synapse, self.chemical_synapse) {
+            (true, false) => self.run_lattices_with_electrical_synapses(iterations).map_err(Into::into),
+            (false, true) => todo!(),
+            (true, true) => todo!(),
             (false, false) => Ok(()),
         }
     }
