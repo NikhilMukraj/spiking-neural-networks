d2e58a361047ade898be606443dd528665f0a016
Nikhiljag <nikhilmukraj@gmail.com>
2024-12-26 13:33:21 -0500

    conversion code works as intended

--- backend/tests/interleaving_graph_conversion.rs
+++ backend/tests/interleaving_graph_conversion.rs
@@ -4,7 +4,9 @@ mod tests {
     use opencl3::{command_queue::{CommandQueue, CL_QUEUE_PROFILING_ENABLE, CL_QUEUE_SIZE}, context::Context, device::{get_all_devices, Device, CL_DEVICE_TYPE_GPU}};
     use rand::Rng;
     use spiking_neural_networks::{
-        error::{GPUError, SpikingNeuralNetworksError}, graph::InterleavingGraphGPU, neuron::{
+        error::{GPUError, SpikingNeuralNetworksError},
+        graph::{AdjacencyMatrix, Graph, GraphPosition, InterleavingGraphGPU}, 
+        neuron::{
             integrate_and_fire::QuadraticIntegrateAndFireNeuron, Lattice, LatticeNetwork
         }
     };
@@ -85,7 +87,45 @@ mod tests {
 
         let connecting_graph = network.get_connecting_graph();
 
-        let _gpu_graph = InterleavingGraphGPU::convert_to_gpu(&context, &queue, &lattices, connecting_graph)?;
+        let gpu_graph = InterleavingGraphGPU::convert_to_gpu(&context, &queue, &lattices, connecting_graph)?;
+
+        let mut editable_connecting_graph = AdjacencyMatrix::<GraphPosition, f32>::default();
+        let mut editable_lattices: HashMap<usize, (_, _)> = network.get_lattices()
+            .iter()
+            .map(|(key, value)| 
+                (*key, (value.cell_grid(), AdjacencyMatrix::<(usize, usize), f32>::default()))
+            )
+            .collect();
+
+        InterleavingGraphGPU::convert_to_cpu(
+            &queue, &gpu_graph, &mut editable_lattices, &mut editable_connecting_graph
+        )?;
+
+        for row in editable_connecting_graph.matrix.iter() {
+            for i in row {
+                assert!(i.is_none());
+            }
+        }
+
+        for i in network.get_all_ids() {
+            let actual_index_to_position = &editable_lattices.get(&i).unwrap().1.index_to_position;
+            let expected_index_to_position = &lattices.get(&i).unwrap().1.index_to_position;
+            assert_eq!(
+                actual_index_to_position, 
+                expected_index_to_position,
+            );
+
+            for pre in actual_index_to_position.values() {
+                for post in actual_index_to_position.values() {
+                    let actual_weight = editable_lattices.get(&i).unwrap().1.lookup_weight(pre, post)
+                        .unwrap();
+                    let expected_weight = lattices.get(&i).unwrap().1.lookup_weight(pre, post)
+                        .unwrap();
+
+                    assert_eq!(actual_weight, expected_weight);
+                }
+            }
+        }
 
         Ok(())
     }
