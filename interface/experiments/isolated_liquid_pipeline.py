bff96e020443e906c3c6442e55d5073ecaea5d0c
Nikhiljag <nikhilmukraj@gmail.com>
2025-01-01 19:10:37 -0500

    moving functions

--- interface/experiments/isolated_liquid_pipeline.py
+++ interface/experiments/isolated_liquid_pipeline.py
@@ -1,10 +1,3 @@
 # generate liquid with random weights, one exc group and one inh group
 # vary glu, gaba conductance and clearance
 # stdp with mnist input potentially
-
-# import numpy as np
-
-# # spectral radius of liquid should be ~1
-# def spectral_radius(A):
-#     eigenvalues = np.linalg.eigvals(A)
-#     return np.max(np.abs(eigenvalues))
