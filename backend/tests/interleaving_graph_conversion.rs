421f524ba5092d4de2c686c23f595865be066f4a
Nikhiljag <nikhilmukraj@gmail.com>
2025-01-03 17:25:31 -0500

    more rigorious testing

--- backend/tests/interleaving_graph_conversion.rs
+++ backend/tests/interleaving_graph_conversion.rs
@@ -7,7 +7,14 @@ mod tests {
         error::{GPUError, SpikingNeuralNetworksError},
         graph::{AdjacencyMatrix, Graph, GraphPosition, InterleavingGraphGPU}, 
         neuron::{
-            integrate_and_fire::QuadraticIntegrateAndFireNeuron, iterate_and_spike::{ApproximateNeurotransmitter, IonotropicNeurotransmitterType}, spike_train::{DeltaDiracRefractoriness, PoissonNeuron}, Lattice, LatticeNetwork, SpikeTrainGridHistory, SpikeTrainLattice
+            integrate_and_fire::QuadraticIntegrateAndFireNeuron, 
+            iterate_and_spike::{
+                ApproximateNeurotransmitter, ApproximateReceptor, IonotropicNeurotransmitterType
+            }, 
+            spike_train::{DeltaDiracRefractoriness, PoissonNeuron}, 
+            plasticity::STDP,
+            Lattice, LatticeNetwork, SpikeTrainGrid, SpikeTrainGridHistory, SpikeTrainLattice,
+            GridVoltageHistory,
         }
     };
 
@@ -305,13 +312,229 @@ mod tests {
         Ok(())
     }
 
-    // #[test]
-    // pub fn test_graph_conversion_spike_train_lattices() -> Result<(), SpikingNeuralNetworksError> {
+    #[test]
+    pub fn test_graph_conversion_spike_train_lattices() -> Result<(), SpikingNeuralNetworksError> {
+        let base_spike_train: PoissonNeuron<
+            IonotropicNeurotransmitterType, ApproximateNeurotransmitter, DeltaDiracRefractoriness
+        > = PoissonNeuron::default();
+
+        let mut spike_train_lattice = SpikeTrainLattice::default_impl();
+        spike_train_lattice.id = 1;
+
+        spike_train_lattice.populate(&base_spike_train, 3, 3);
+
+        #[allow(clippy::type_complexity)]
+        let mut network: LatticeNetwork<
+            QuadraticIntegrateAndFireNeuron<ApproximateNeurotransmitter, ApproximateReceptor>, 
+            AdjacencyMatrix<(usize, usize), f32>, 
+            GridVoltageHistory, 
+            _, 
+            SpikeTrainGridHistory, 
+            AdjacencyMatrix<GraphPosition, f32>, 
+            STDP, 
+            IonotropicNeurotransmitterType
+        > = LatticeNetwork::default();
+        network.parallel = true;
+        network.add_spike_train_lattice(spike_train_lattice)?;
+
+        let device_id = *get_all_devices(CL_DEVICE_TYPE_GPU)
+            .expect("Could not get GPU devices")
+            .first()
+            .expect("No GPU found");
+        let device = Device::new(device_id);
+
+        let context = match Context::from_device(&device) {
+            Ok(value) => value,
+            Err(_) => return Err(Into::into(GPUError::GetDeviceFailure)),
+        };
+
+        let queue = match CommandQueue::create_default_with_properties(
+                &context, 
+                CL_QUEUE_PROFILING_ENABLE,
+                CL_QUEUE_SIZE,
+            ) {
+                Ok(value) => value,
+                Err(_) => return Err(Into::into(GPUError::GetDeviceFailure)),
+            };
+
+        let connecting_graph: &AdjacencyMatrix<GraphPosition, f32> = network.get_connecting_graph();
+
+        let gpu_graph = InterleavingGraphGPU::convert_to_gpu(
+            &context, &queue, network.get_lattices(), network.get_spike_train_lattices(), connecting_graph
+        )?;
+
+        let mut editable_connecting_graph = AdjacencyMatrix::<GraphPosition, f32>::default();
+        #[allow(clippy::type_complexity)]
+        let mut editable_lattices: HashMap<
+            usize,
+            Lattice<
+                QuadraticIntegrateAndFireNeuron<ApproximateNeurotransmitter, ApproximateReceptor>,
+                AdjacencyMatrix<(usize, usize), f32>,
+                GridVoltageHistory,
+                STDP,
+                IonotropicNeurotransmitterType
+            >
+        > = HashMap::new();
+        let mut editable_spike_train_lattices: HashMap<usize, _> = network.get_all_spike_train_lattice_ids()
+            .iter()
+            .map(|key| 
+                (*key, {
+                    let lattice_to_copy = network.get_spike_train_lattice(key).unwrap();
+                    let mut current_lattice = SpikeTrainLattice::default_impl();
+                    current_lattice.set_id(*key);
+                    current_lattice.populate(
+                        &base_spike_train,
+                        lattice_to_copy.spike_train_grid().len(),
+                        lattice_to_copy.spike_train_grid().first().unwrap_or(&vec![]).len(),
+                    );
+
+                    current_lattice
+                })
+            )
+            .collect();
+
+        InterleavingGraphGPU::convert_to_cpu(
+            &queue, &gpu_graph, &mut editable_lattices, &mut editable_spike_train_lattices, &mut editable_connecting_graph
+        )?;
+
+        assert!(editable_lattices.is_empty());
+        for row in editable_connecting_graph.matrix.iter() {
+            for value in row {
+                assert!(value.is_none());
+            }
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    pub fn test_graph_conversion_spike_train_and_regular_lattices() -> Result<(), SpikingNeuralNetworksError> {
+        let base_neuron = QuadraticIntegrateAndFireNeuron {
+            gap_conductance: 0.1,
+            ..QuadraticIntegrateAndFireNeuron::default_impl()
+        };
+
+        let mut lattice1 = Lattice::default_impl();
         
-    // }
+        lattice1.populate(
+            &base_neuron, 
+            2, 
+            2, 
+        );
+    
+        lattice1.connect(&connection_conditional, None);
+        lattice1.apply(|neuron: &mut _| {
+            let mut rng = rand::thread_rng();
+            neuron.current_voltage = rng.gen_range(neuron.v_init..=neuron.v_th);
+        });
+        lattice1.update_grid_history = true;
 
-    // #[test]
-    // pub fn test_graph_conversion_spike_train_and_regular_lattices() -> Result<(), SpikingNeuralNetworksError> {
+        let base_spike_train: PoissonNeuron<
+            IonotropicNeurotransmitterType, ApproximateNeurotransmitter, DeltaDiracRefractoriness
+        > = PoissonNeuron::default();
 
-    // }
+        let mut spike_train_lattice = SpikeTrainLattice::default_impl();
+        spike_train_lattice.id = 1;
+
+        spike_train_lattice.populate(&base_spike_train, 3, 3);
+
+        let mut network = LatticeNetwork::default();
+        network.parallel = true;
+        network.add_lattice(lattice1)?;
+        network.add_spike_train_lattice(spike_train_lattice)?;
+
+        let device_id = *get_all_devices(CL_DEVICE_TYPE_GPU)
+            .expect("Could not get GPU devices")
+            .first()
+            .expect("No GPU found");
+        let device = Device::new(device_id);
+
+        let context = match Context::from_device(&device) {
+            Ok(value) => value,
+            Err(_) => return Err(Into::into(GPUError::GetDeviceFailure)),
+        };
+
+        let queue = match CommandQueue::create_default_with_properties(
+                &context, 
+                CL_QUEUE_PROFILING_ENABLE,
+                CL_QUEUE_SIZE,
+            ) {
+                Ok(value) => value,
+                Err(_) => return Err(Into::into(GPUError::GetDeviceFailure)),
+            };
+
+        let connecting_graph: &AdjacencyMatrix<GraphPosition, f32> = network.get_connecting_graph();
+
+        let gpu_graph = InterleavingGraphGPU::convert_to_gpu(
+            &context, &queue, network.get_lattices(), network.get_spike_train_lattices(), connecting_graph
+        )?;
+
+        let mut editable_connecting_graph = AdjacencyMatrix::<GraphPosition, f32>::default();
+        let mut editable_lattices: HashMap<usize, _> = network.get_all_lattice_ids()
+            .iter()
+            .map(|key| 
+                (*key, {
+                    let lattice_to_copy = network.get_lattice(key).unwrap();
+                    let mut current_lattice = Lattice::default_impl();
+                    current_lattice.set_id(*key);
+                    current_lattice.populate(
+                        &base_neuron, 
+                        lattice_to_copy.cell_grid().len(),
+                        lattice_to_copy.cell_grid().first().unwrap_or(&vec![]).len(),
+                    );
+
+                    current_lattice
+                })
+            )
+            .collect();
+        let mut editable_spike_train_lattices: HashMap<usize, _> = network.get_all_spike_train_lattice_ids()
+            .iter()
+            .map(|key| 
+                (*key, {
+                    let lattice_to_copy = network.get_spike_train_lattice(key).unwrap();
+                    let mut current_lattice = SpikeTrainLattice::default_impl();
+                    current_lattice.set_id(*key);
+                    current_lattice.populate(
+                        &base_spike_train,
+                        lattice_to_copy.spike_train_grid().len(),
+                        lattice_to_copy.spike_train_grid().first().unwrap_or(&vec![]).len(),
+                    );
+
+                    current_lattice
+                })
+            )
+            .collect();
+
+        InterleavingGraphGPU::convert_to_cpu(
+            &queue, &gpu_graph, &mut editable_lattices, &mut editable_spike_train_lattices, &mut editable_connecting_graph
+        )?;
+
+        for row in editable_connecting_graph.matrix.iter() {
+            for i in row {
+                assert!(i.is_none());
+            }
+        }
+
+        for i in network.get_all_lattice_ids() {
+            let actual_index_to_position = &editable_lattices.get(&i).unwrap().graph().index_to_position;
+            let expected_index_to_position = &network.get_lattice(&i).unwrap().graph().index_to_position;
+            assert_eq!(
+                actual_index_to_position, 
+                expected_index_to_position,
+            );
+
+            for pre in actual_index_to_position.values() {
+                for post in actual_index_to_position.values() {
+                    let actual_weight = editable_lattices.get(&i).unwrap().graph().lookup_weight(pre, post)
+                        .unwrap();
+                    let expected_weight = network.get_lattice(&i).unwrap().graph().lookup_weight(pre, post)
+                        .unwrap();
+
+                    assert_eq!(actual_weight, expected_weight);
+                }
+            }
+        }
+
+        Ok(())
+    }
 }
