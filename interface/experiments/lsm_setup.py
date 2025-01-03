2684aeb8f5a2a91ca9c7e3a8c402ed59e675760f
Nikhiljag <nikhilmukraj@gmail.com>
2025-01-02 21:03:44 -0500

    new function

--- interface/experiments/lsm_setup.py
+++ interface/experiments/lsm_setup.py
@@ -1,10 +1,15 @@
 import numpy as np
 
 
-# spectral radius of liquid should be ~1
 def spectral_radius(A):
     eigenvalues = np.linalg.eigvals(A)
     return np.max(np.abs(eigenvalues))
 
-# def generate_liquid_weights(rows, cols):
-#   ...
+def generate_liquid_weights(rows, cols, minimum=0, maximum=1):
+    x = np.random.uniform(minimum, maximum, (rows, cols))
+    np.fill_diagonal(x, 0)
+
+    # spectral radius of liquid should be ~1
+    x /= spectral_radius(x)
+
+    return x
