bb64d5e28ced1ca7f59c638a145a30635de6e328
Nikhiljag <nikhilmukraj@gmail.com>
2025-01-04 18:29:16 -0500

    new params

--- interface/experiments/lsm_setup.py
+++ interface/experiments/lsm_setup.py
@@ -5,7 +5,7 @@ def spectral_radius(A):
     eigenvalues = np.linalg.eigvals(A)
     return np.max(np.abs(eigenvalues))
     
-def generate_liquid_weights(size, minimum=0, maximum=1, connectivity=0.25):
+def generate_liquid_weights(size, minimum=0, maximum=1, connectivity=0.25, scalar=0.5):
     w = np.zeros((size, size))
     
     connections = np.random.rand(size, size) < connectivity
@@ -16,6 +16,6 @@ def generate_liquid_weights(size, minimum=0, maximum=1, connectivity=0.25):
     np.fill_diagonal(w, 0)
     
     # spectral radius near 1
-    w /= spectral_radius(w) * 0.5
+    w /= spectral_radius(w) * scalar
 
     return w
