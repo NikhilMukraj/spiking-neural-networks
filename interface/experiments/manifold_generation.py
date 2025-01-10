e4359a9ae1aa3f961c35b9b9c8b91aecb7c5baa3
Nikhiljag <nikhilmukraj@gmail.com>
2025-01-09 19:13:21 -0500

    manifold generation

---
+++ interface/experiments/manifold_generation.py
@@ -0,0 +1,5 @@
+# generate auto-associative attractor
+# determine firing rates of each neuron when exposed to each cue at varying distortions 
+# for n trials
+# use umap 3d embedding to visualize distance between attractor states
+# save firing rate data and embedding
