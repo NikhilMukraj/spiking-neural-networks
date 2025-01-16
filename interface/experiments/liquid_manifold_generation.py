6faa15d006638e8f322de478a7aba35ca36171f2
Nikhiljag <nikhilmukraj@gmail.com>
2025-01-15 19:34:45 -0500

    notes

--- interface/experiments/liquid_manifold_generation.py
+++ interface/experiments/liquid_manifold_generation.py
@@ -2,3 +2,5 @@
 # input random digit from mnist
 # record trials
 # generate umap based on neural activity
+# determine which neurons contribute to which digits by
+# inputting differing firing rates into reducer to see where they land
