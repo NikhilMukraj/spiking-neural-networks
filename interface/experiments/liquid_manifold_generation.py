aaf8bae55ceb1918b06402f1160d3023a30a9529
Nikhiljag <nikhilmukraj@gmail.com>
2025-01-20 17:06:49 -0500

    work on liquid manifold generation

--- interface/experiments/liquid_manifold_generation.py
+++ interface/experiments/liquid_manifold_generation.py
@@ -28,108 +28,110 @@ cue_to_liquid = np.array([
     for _ in range(digits_size * spacing_term)
 ])
 
-# map cue to liquid with above weight matrix
+# generate current state dictionary to determine parameters for simulation
 
-# for current_state in tqdm(all_states):
-#     for trial in range(parsed_toml['simulation_parameters']['trials']):
-#         w = generate_liquid_weights(
-#             num, connectivity=current_state['connectivity'], scalar=current_state['internal_scalar']
+# attach firing rate data to a target and the inputted pattern
+# simulation_output = {}
+
+# for current_digit, current_class in zip(data, target):
+#     w = generate_liquid_weights(
+#         num, connectivity=current_state['connectivity'], scalar=current_state['internal_scalar']
+#     )
+
+#     if not parsed_toml['simulation_parameters']['exc_only']:
+#         w_inh = generate_liquid_weights(
+#             inh_num, connectivity=current_state['inh_connectivity'], scalar=current_state['inh_internal_scalar']
 #         )
 
-#         if not parsed_toml['simulation_parameters']['exc_only']:
-#             w_inh = generate_liquid_weights(
-#                 inh_num, connectivity=current_state['inh_connectivity'], scalar=current_state['inh_internal_scalar']
-#             )
+#     start_firing = generate_start_firing(current_state['cue_firing_rate'])
 
-#         start_firing = generate_start_firing(current_state['cue_firing_rate'])
+#     glu_neuro = ln.ApproximateNeurotransmitter(clearance_constant=current_state['glu_clearance'])
+#     exc_neurotransmitters = ln.DopaGluGABAApproximateNeurotransmitters()
+#     exc_neurotransmitters.set_neurotransmitter(ln.DopaGluGABANeurotransmitterType.Glutamate, glu_neuro)
 
-#         glu_neuro = ln.ApproximateNeurotransmitter(clearance_constant=current_state['glu_clearance'])
-#         exc_neurotransmitters = ln.DopaGluGABAApproximateNeurotransmitters()
-#         exc_neurotransmitters.set_neurotransmitter(ln.DopaGluGABANeurotransmitterType.Glutamate, glu_neuro)
+#     gaba_neuro = ln.ApproximateNeurotransmitter(clearance_constant=current_state['gabaa_clearance'])
+#     inh_neurotransmitters = ln.DopaGluGABAApproximateNeurotransmitters()
+#     inh_neurotransmitters.set_neurotransmitter(ln.DopaGluGABANeurotransmitterType.GABA, gaba_neuro)
 
-#         gaba_neuro = ln.ApproximateNeurotransmitter(clearance_constant=current_state['gabaa_clearance'])
-#         inh_neurotransmitters = ln.DopaGluGABAApproximateNeurotransmitters()
-#         inh_neurotransmitters.set_neurotransmitter(ln.DopaGluGABANeurotransmitterType.GABA, gaba_neuro)
+#     glu = ln.GlutamateReceptor()
+    
+#     glu.ampa_g = current_state['nmda_g']
+#     glu.nmda_g = current_state['ampa_g']
 
-#         glu = ln.GlutamateReceptor()
-        
-#         glu.ampa_g = current_state['nmda_g']
-#         glu.nmda_g = current_state['ampa_g']
+#     gaba = ln.GABAReceptor()
+#     gaba.g = current_state['gabaa_g']
 
-#         gaba = ln.GABAReceptor()
-#         gaba.g = current_state['gabaa_g']
+#     receptors = ln.DopaGluGABAReceptors()
 
-#         receptors = ln.DopaGluGABAReceptors()
+#     receptors.set_receptor(ln.DopaGluGABANeurotransmitterType.Glutamate, glu)
+#     receptors.set_receptor(ln.DopaGluGABANeurotransmitterType.GABA, gaba)
 
-#         receptors.set_receptor(ln.DopaGluGABANeurotransmitterType.Glutamate, glu)
-#         receptors.set_receptor(ln.DopaGluGABANeurotransmitterType.GABA, gaba)
+#     exc_neuron = ln.DopaIzhikevichNeuron()
+#     exc_neuron.set_neurotransmitters(exc_neurotransmitters)
+#     exc_neuron.set_receptors(receptors)
 
-#         exc_neuron = ln.DopaIzhikevichNeuron()
-#         exc_neuron.set_neurotransmitters(exc_neurotransmitters)
-#         exc_neuron.set_receptors(receptors)
+#     inh_neuron = ln.DopaIzhikevichNeuron()
+#     inh_neuron.set_neurotransmitters(inh_neurotransmitters)
+#     inh_neuron.set_receptors(receptors)
 
-#         inh_neuron = ln.DopaIzhikevichNeuron()
-#         inh_neuron.set_neurotransmitters(inh_neurotransmitters)
-#         inh_neuron.set_receptors(receptors)
+#     poisson_neuron = ln.DopaPoissonNeuron()
+#     poisson_neuron.set_neurotransmitters(exc_neurotransmitters)
 
-#         poisson_neuron = ln.DopaPoissonNeuron()
-#         poisson_neuron.set_neurotransmitters(exc_neurotransmitters)
+#     exc_lattice = ln.DopaIzhikevichLattice(0)
+#     exc_lattice.populate(exc_neuron, exc_n, exc_n)
+#     exc_lattice.apply(setup_neuron)
+#     position_to_index = exc_lattice.position_to_index
+#     exc_lattice.connect(
+#         lambda x, y: bool(float(w[position_to_index[x]][position_to_index[y]]) != 0), 
+#         lambda x, y: float(w[position_to_index[x]][position_to_index[y]]),
+#     )
+#     exc_lattice.update_grid_history = True
 
-#         exc_lattice = ln.DopaIzhikevichLattice(0)
-#         exc_lattice.populate(exc_neuron, exc_n, exc_n)
-#         exc_lattice.apply(setup_neuron)
-#         position_to_index = exc_lattice.position_to_index
-#         exc_lattice.connect(
-#             lambda x, y: bool(float(w[position_to_index[x]][position_to_index[y]]) != 0), 
-#             lambda x, y: float(w[position_to_index[x]][position_to_index[y]]),
+#     spike_train_lattice = ln.DopaPoissonLattice(1)
+#     spike_train_lattice.populate(poisson_neuron, exc_n, exc_n)
+
+#     if not parsed_toml['simulation_parameters']['exc_only']:
+#         inh_lattice = ln.DopaIzhikevichLattice(2)
+#         inh_lattice.populate(inh_neuron, inh_n, inh_n)
+#         inh_lattice.apply(setup_neuron)
+#         position_to_index = inh_lattice.position_to_index
+#         inh_lattice.connect(
+#             lambda x, y: bool(float(w_inh[position_to_index[x]][position_to_index[y]]) != 0), 
+#             lambda x, y: float(w_inh[position_to_index[x]][position_to_index[y]]),
 #         )
-#         exc_lattice.update_grid_history = True
+#         # inh_lattice.update_grid_history = True
 
-#         spike_train_lattice = ln.DopaPoissonLattice(1)
-#         spike_train_lattice.populate(poisson_neuron, exc_n, exc_n)
+#         network = ln.DopaIzhikevichNetwork.generate_network(
+#             [exc_lattice, inh_lattice], [spike_train_lattice],
+#         )
+#     else:
+#         network = ln.DopaIzhikevichNetwork.generate_network(
+#             [exc_lattice], [spike_train_lattice],
+#         )
 
-#         if not parsed_toml['simulation_parameters']['exc_only']:
-#             inh_lattice = ln.DopaIzhikevichLattice(2)
-#             inh_lattice.populate(inh_neuron, inh_n, inh_n)
-#             inh_lattice.apply(setup_neuron)
-#             position_to_index = inh_lattice.position_to_index
-#             inh_lattice.connect(
-#                 lambda x, y: bool(float(w_inh[position_to_index[x]][position_to_index[y]]) != 0), 
-#                 lambda x, y: float(w_inh[position_to_index[x]][position_to_index[y]]),
-#             )
-#             # inh_lattice.update_grid_history = True
+#     network.set_dt(parsed_toml['simulation_parameters']['dt'])
+#     network.parallel = True
 
-#             network = ln.DopaIzhikevichNetwork.generate_network(
-#                 [exc_lattice, inh_lattice], [spike_train_lattice],
-#             )
-#         else:
-#             network = ln.DopaIzhikevichNetwork.generate_network(
-#                 [exc_lattice], [spike_train_lattice],
-#             )
-
-#         network.set_dt(parsed_toml['simulation_parameters']['dt'])
-#         network.parallel = True
-
-#         if not parsed_toml['simulation_parameters']['exc_only']:
-#             network.connect(
-#                 2, 
-#                 0, 
-#                 lambda x, y: np.random.uniform(0, 1) < current_state['inh_to_exc_connectivity'], 
-#                 lambda x, y: current_state['inh_to_exc_weight'],
-#             )
-#             network.connect(
-#                 0, 
-#                 2, 
-#                 lambda x, y: np.random.uniform(0, 1) < current_state['exc_to_inh_connectivity'],
-#                 lambda x, y: current_state['exc_to_inh_weight'],
-#             )
-
+#     if not parsed_toml['simulation_parameters']['exc_only']:
 #         network.connect(
-#             1, 
+#             2, 
 #             0, 
-#             lambda x, y: np.random.uniform(0, 1) < current_state['spike_train_connectivity'], 
-#             lambda x, y: current_state['spike_train_to_exc']
+#             lambda x, y: np.random.uniform(0, 1) < current_state['inh_to_exc_connectivity'], 
+#             lambda x, y: current_state['inh_to_exc_weight'],
+#         )
+#         network.connect(
+#             0, 
+#             2, 
+#             lambda x, y: np.random.uniform(0, 1) < current_state['exc_to_inh_connectivity'],
+#             lambda x, y: current_state['exc_to_inh_weight'],
 #         )
 
-#         network.electrical_synapse = False
-#         network.chemical_synapse = True
+#     network.connect(
+#         1, 
+#         0, 
+#         lambda x, y: cue_to_liquid[x[0] * (digits_size * spacing_term) + x[1]], 
+#         lambda x, y: current_state['spike_train_to_exc']
+#     )
+
+#     network.electrical_synapse = False
+#     network.chemical_synapse = True
