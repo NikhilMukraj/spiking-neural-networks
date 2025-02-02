aff311240f85a19d8bff6945a13934a6ed18912c
Nikhiljag <nikhilmukraj@gmail.com>
2025-02-02 18:03:58 -0500

    new if statement test

--- build_test/nb_macro/tests/if_statements.rs
+++ build_test/nb_macro/tests/if_statements.rs
@@ -43,6 +43,21 @@ mod test {
                 [end]
             [end]
     [end]
+
+    [neuron]
+        type: ElseBasicIntegrateAndFire
+        vars: e = 0, v_reset = -75, v_th = -55, flag = 0
+        on_spike: 
+            v = v_reset
+        spike_detection: v >= v_th
+        on_iteration:
+            dv/dt = (v - e) + i
+            [if] i < 0 [then]
+                flag = 1
+            [else]
+                flag = 2
+            [end]
+    [end]
     "#);
 
     #[test]
@@ -114,4 +129,40 @@ mod test {
         }
     }
 
+    #[test]
+    pub fn test_if_else_statement() {
+        let voltages = [-50., -40., -30., -20., -10., 0., 10., 20., 30., 40., 50.];
+
+        for i in voltages {
+            let mut test_output: Vec<f32> = vec![];
+            let mut reference_output: Vec<f32> = vec![];
+
+            let mut to_test: ElseBasicIntegrateAndFire<ApproximateNeurotransmitter, ApproximateReceptor> = 
+                ElseBasicIntegrateAndFire::default();
+            let mut reference_neuron: ReferenceIntegrateAndFire<ApproximateNeurotransmitter, ApproximateReceptor> = 
+                ReferenceIntegrateAndFire::default();
+
+            assert_eq!(to_test.flag, 0.);
+
+            for _ in 0..1000 {
+                let _ = to_test.iterate_and_spike(i);
+                test_output.push(to_test.current_voltage);
+                let _ = reference_neuron.iterate_and_spike(i);
+                reference_output.push(reference_neuron.current_voltage);
+
+                if i < 0. {
+                    assert_eq!(to_test.flag, 1.)
+                } else {
+                    assert_eq!(to_test.flag, 2.)
+                }
+            }
+
+            assert_eq!(test_output, reference_output);
+        }
+    }
+
+    // #[test]
+    // pub fn test_if_if_else_statement() {
+
+    // }
 }
