1bd1e4f7c2680319589d8da987d776f8d793517e
Nikhiljag <nikhilmukraj@gmail.com>
2025-02-07 18:35:57 -0500

    new ast

--- build_test/nb_macro/src/pest_ast/mod.rs
+++ build_test/nb_macro/src/pest_ast/mod.rs
@@ -115,10 +115,23 @@ ion_channel_definition = {
 neurotransmitter_kinetics_definition= {"[neurotransmitter_kinetics]" ~ NEWLINE ~ (type_def | vars_with_default_def | on_iteration_def){3,} ~ "[end]" }
 receptor_kinetics_definition = {"[receptor_kinetics]" ~ NEWLINE ~ (type_def | vars_with_default_def | on_iteration_def){3,} ~ "[end]" }
 
+neurotransmitter_def = { "neurotransmitter:" ~ " "* ~ name ~ NEWLINE+ }
+receptors_definition = {
+	"[receptors]" ~ NEWLINE+ ~ 
+		(
+			type_def | vars_with_default_def | 
+			(
+				neurotransmitter_def ~ NEWLINE* ~ vars_with_default_def ~ 
+				NEWLINE* ~ on_iteration_def ~ NEWLINE*
+			)+
+		){2,3} ~ WHITESPACE* ~ "[end]"
+}
+
 full = _{
 	SOI ~ NEWLINE* ~ ((
 		neuron_definition | ion_channel_definition | 
-		neurotransmitter_kinetics_definition | receptor_kinetics_definition 
+		neurotransmitter_kinetics_definition | receptor_kinetics_definition |
+        receptors_definition
 	)+ ~ NEWLINE*
 	)* ~ EOI
 }
