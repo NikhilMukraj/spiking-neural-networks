8224fca782c0c7ba6b21b756e1922115e3ea2088
Nikhiljag <nikhilmukraj@gmail.com>
2025-02-08 14:53:45 -0500

    start of receptor testing

--- build_test/nb_macro/tests/receptors.rs
+++ build_test/nb_macro/tests/receptors.rs
@@ -1,6 +1,15 @@
-// [receptors]
-//     type: BasicReceptors
-//     neurotransmitter: X
-//     on_iteration:
-//         current = g * r * (v - e)
-// [end]
+#[cfg(test)]
+mod test {
+    use nb_macro::neuron_builder;
+
+
+    neuron_builder!(r#"
+    [receptors]
+        type: BasicReceptors
+        neurotransmitter: X
+        vars: g = 1, e = 0
+        on_iteration:
+            current = g * r * (v - e)
+    [end]
+    "#);
+}
