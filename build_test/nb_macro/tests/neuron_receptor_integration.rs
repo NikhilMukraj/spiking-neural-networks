0687638709b8898981ffae2f461c597bd0064ad5
Nikhiljag <nikhilmukraj@gmail.com>
2025-02-15 14:33:36 -0500

    more tests, tests pass

--- build_test/nb_macro/tests/neuron_receptor_integration.rs
+++ build_test/nb_macro/tests/neuron_receptor_integration.rs
@@ -6,6 +6,7 @@
 
 mod shared_receptors;
 
+#[allow(clippy::assign_op_pattern)]
 #[cfg(test)]
 mod test {
     use std::collections::HashMap;
@@ -16,7 +17,6 @@ mod test {
         MixedReceptorsNeurotransmitterType, MultipleReceptors, MultipleReceptorsNeurotransmitterType, 
         MultipleReceptorsType,
     };
-
 
     neuron_builder!(r#"
     [neuron]
@@ -39,6 +39,23 @@ mod test {
         spike_detection: v >= v_th
         on_iteration:
             dv/dt = (v - e) + i
+    [end]
+
+    [neuron]
+        type: ElectroChemicalIntegrateAndFire
+        receptors: MultipleReceptors
+        vars: e = 0, v_reset = -75, v_th = -55, modifier = 1
+        on_spike: 
+            v = v_reset
+        spike_detection: v >= v_th
+        on_iteration:
+            dv/dt = (v - e) + i
+        on_electrochemical_iteration:
+            receptors.update_receptor_kinetics(t, dt)
+            receptors.set_receptor_currents(v, dt)
+            dv/dt = (v - e) + i
+            v = (modifier * -receptors.get_receptor_currents(dt, c_m)) + v
+            synaptic_neurotransmitters.apply_t_changes()
     [end]
     "#);
 
@@ -132,5 +149,133 @@ mod test {
             sum_bool(spiking_data.get(&(true, false)).unwrap()) < 
             sum_bool(spiking_data.get(&(true, true)).unwrap())
         );
+    }
+
+    #[test]
+    fn test_custom_electrochemical_same() {
+        for t in [0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.]  {
+            for i in (-50..50).step_by(10) {
+                for (has_a, has_b) in [(false, false), (true, false), (true, true)] {
+                    let mut to_test: ElectroChemicalIntegrateAndFire::<ApproximateNeurotransmitter, ApproximateReceptor> = 
+                        ElectroChemicalIntegrateAndFire::default();
+                    let mut ref_neuron: MultiIntegrateAndFire::<ApproximateNeurotransmitter, ApproximateReceptor> = 
+                        MultiIntegrateAndFire::default();
+
+                    to_test.dt = 1.;
+                    ref_neuron.dt = 1.;
+        
+                    if has_a {
+                        to_test.receptors.insert(
+                            MultipleReceptorsNeurotransmitterType::A,
+                            MultipleReceptorsType::A(AReceptor { g: 2., ..AReceptor::default() }),
+                        ).unwrap();
+                        ref_neuron.receptors.insert(
+                            MultipleReceptorsNeurotransmitterType::A,
+                            MultipleReceptorsType::A(AReceptor { g: 2., ..AReceptor::default() }),
+                        ).unwrap();
+                    }
+                    if has_b {
+                        to_test.receptors.insert(
+                            MultipleReceptorsNeurotransmitterType::B,
+                            MultipleReceptorsType::B(BReceptor { g: 2., ..BReceptor::default() }),
+                        ).unwrap();
+                        ref_neuron.receptors.insert(
+                            MultipleReceptorsNeurotransmitterType::B,
+                            MultipleReceptorsType::B(BReceptor { g: 2., ..BReceptor::default() }),
+                        ).unwrap();
+                    }
+        
+                    let conc = HashMap::from([
+                        (MultipleReceptorsNeurotransmitterType::A, t),
+                        (MultipleReceptorsNeurotransmitterType::B, t),
+                    ]);
+
+                    for _ in 0..1000 {
+                        let is_spiking1 = to_test.iterate_with_neurotransmitter_and_spike(
+                            i as f32, &conc
+                        );
+                        let is_spiking2 = ref_neuron.iterate_with_neurotransmitter_and_spike(
+                            i as f32, &conc
+                        );
+
+                        assert_eq!(is_spiking1, is_spiking2);
+
+                        if to_test.current_voltage.is_finite() && ref_neuron.current_voltage.is_finite() {
+                            assert!(
+                                ((to_test.current_voltage - ref_neuron.current_voltage) / ref_neuron.current_voltage).abs() < 0.01,
+                                "{} != {}",
+                                to_test.current_voltage,
+                                ref_neuron.current_voltage,
+                            );
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn test_custom_electrochemical_differing() {
+        let mut total_spikes1 = 0;
+        let mut total_spikes2 = 0;
+
+        for t in [0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.]  {
+            for i in (-50..50).step_by(10) {
+                for (has_a, has_b) in [(false, false), (true, false), (true, true)] {
+                    let mut to_test: ElectroChemicalIntegrateAndFire::<ApproximateNeurotransmitter, ApproximateReceptor> = 
+                        ElectroChemicalIntegrateAndFire::default();
+                    let mut ref_neuron: MultiIntegrateAndFire::<ApproximateNeurotransmitter, ApproximateReceptor> = 
+                        MultiIntegrateAndFire::default();
+
+                    to_test.modifier = 3.;
+                    to_test.dt = 1.;
+                    ref_neuron.dt = 1.;
+        
+                    if has_a {
+                        to_test.receptors.insert(
+                            MultipleReceptorsNeurotransmitterType::A,
+                            MultipleReceptorsType::A(AReceptor { g: 2., ..AReceptor::default() }),
+                        ).unwrap();
+                        ref_neuron.receptors.insert(
+                            MultipleReceptorsNeurotransmitterType::A,
+                            MultipleReceptorsType::A(AReceptor { g: 2., ..AReceptor::default() }),
+                        ).unwrap();
+                    }
+                    if has_b {
+                        to_test.receptors.insert(
+                            MultipleReceptorsNeurotransmitterType::B,
+                            MultipleReceptorsType::B(BReceptor { g: 2., ..BReceptor::default() }),
+                        ).unwrap();
+                        ref_neuron.receptors.insert(
+                            MultipleReceptorsNeurotransmitterType::B,
+                            MultipleReceptorsType::B(BReceptor { g: 2., ..BReceptor::default() }),
+                        ).unwrap();
+                    }
+        
+                    let conc = HashMap::from([
+                        (MultipleReceptorsNeurotransmitterType::A, t),
+                        (MultipleReceptorsNeurotransmitterType::B, t),
+                    ]);
+
+                    for _ in 0..1000 {
+                        let is_spiking1 = to_test.iterate_with_neurotransmitter_and_spike(
+                            i as f32, &conc
+                        );
+                        let is_spiking2 = ref_neuron.iterate_with_neurotransmitter_and_spike(
+                            i as f32, &conc
+                        );
+
+                        if is_spiking1 {
+                            total_spikes1 += 1;
+                        }
+                        if is_spiking2 {
+                            total_spikes2 += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        assert!(total_spikes1 > total_spikes2);
     }
 }
