51ec6e276e2b6b5f91ef56ae98048cb975239e35
Nikhiljag <nikhilmukraj@gmail.com>
2025-02-16 15:25:56 -0500

    new receptor type to test

--- build_test/nb_macro/tests/shared_receptors.rs
+++ build_test/nb_macro/tests/shared_receptors.rs
@@ -15,15 +15,24 @@ neuron_builder!(r#"
 [end]
 
 [receptors]
-        type: MixedReceptors
-        vars: m = 0
-        neurotransmitter: Iono
-        vars: current = 0, g = 1, e = 0
-        on_iteration:
-            current = g * m * r * (v - e)
-        neurotransmitter: Meta
-        vars: s = 1
-        on_iteration:
-            m = s * r
-    [end]
+    type: MixedReceptors
+    vars: m = 0
+    neurotransmitter: Iono
+    vars: current = 0, g = 1, e = 0
+    on_iteration:
+        current = g * m * r * (v - e)
+    neurotransmitter: Meta
+    vars: s = 1
+    on_iteration:
+        m = s * r
+[end]
+
+[receptors]
+    type: CombinedReceptors
+    neurotransmitter: Combined
+    receptors: r1, r2
+    vars: current = 0, g1 = 2, e1 = 0, g2 = 2, e2 = 0
+    on_iteration:
+        current = g1 * r1 * (v - e1) + g2 * r2 * (v - e2)
+[end]
 "#);
