82d67fa55379c1e207ad5977bd20c7d4f2df8249
Nikhiljag <nikhilmukraj@gmail.com>
2025-02-23 17:44:07 -0500

    test macro compiles

--- build_test/nb_macro/tests/gpu_neurotransmitter_kinetics.rs
+++ build_test/nb_macro/tests/gpu_neurotransmitter_kinetics.rs
@@ -1,5 +1,22 @@
-#[cfg(gpu)]
+#[cfg(feature = "gpu")]
 #[cfg(test)]
 mod test {
+    use nb_macro::neuron_builder; 
 
+    neuron_builder!(r#"
+    [neurotransmitter_kinetics]
+        type: BasicNeurotransmitterKinetics
+        vars: t_max = 1, c = 0.001, conc = 0
+        on_iteration:
+            [if] is_spiking [then]
+                conc = t_max
+            [else]
+                conc = 0
+            [end]
+
+            t = t + dt * -c * t + conc
+
+            t = min(max(t, 0), t_max)
+    [end]
+    "#);
 }
