8e6eb607411aab0915ae802e18a802f3bf1fb141
Nikhiljag <nikhilmukraj@gmail.com>
2025-03-05 19:52:40 -0500

    notes

--- interface/experiments/heuristic_parameter_search.py
+++ interface/experiments/heuristic_parameter_search.py
@@ -3,3 +3,4 @@
 #   - Assume that trends are linear and will continue in that direction
 #   - Track which changes contribute to which trends
 #   - Go in direction of desired target
+# Start with Bayesian inference pipeline
