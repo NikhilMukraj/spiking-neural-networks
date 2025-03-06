b9a5c534f9acc95ff441de06eba84555d2ea6d34
Nikhiljag <nikhilmukraj@gmail.com>
2025-03-05 19:52:26 -0500

    heuristic parameter search

---
+++ interface/experiments/heuristic_parameter_search.py
@@ -0,0 +1,5 @@
+#   - Define target behavior
+#     - Probably as a Python function
+#   - Assume that trends are linear and will continue in that direction
+#   - Track which changes contribute to which trends
+#   - Go in direction of desired target
