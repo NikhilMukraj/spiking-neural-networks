66c6b5a0486703fe310de27b136df67e52126efd
Nikhiljag <nikhilmukraj@gmail.com>
2025-03-06 20:33:37 -0500

    rationale

--- interface/experiments/heuristic_parameter_search.py
+++ interface/experiments/heuristic_parameter_search.py
@@ -4,3 +4,8 @@
 #   - Track which changes contribute to which trends
 #   - Go in direction of desired target
 # Start with Bayesian inference pipeline
+
+# key is parameters, value is score
+# when multiple values are found, find linear trend
+# find the most correlated linear trend and move in the direction of closer to target
+analysis = {}
