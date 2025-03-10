edc69705ddaea08291ea8b23f93314271b9f391f
Nikhiljag <nikhilmukraj@gmail.com>
2025-03-10 19:34:42 -0400

    new pipeline

---
+++ interface/experiments/dopamine_liquid_interaction.py
@@ -0,0 +1,5 @@
+# regular disturbing spike train group to liquid
+# second spike train group to liquid or inh
+# option to turn on d1 or d2
+# option to connect second spike train group to either liquid or inh
+# vary parameters to determine effect of stability based on neuromodulation
