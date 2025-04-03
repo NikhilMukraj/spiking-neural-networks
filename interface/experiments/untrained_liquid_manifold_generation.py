af792d6e2142d5d5bd7ba039848322b853367cf0
Nikhiljag <nikhilmukraj@gmail.com>
2025-04-03 19:30:40 -0400

    new pipeline

---
+++ interface/experiments/untrained_liquid_manifold_generation.py
@@ -0,0 +1,4 @@
+# generate a liquid with exc and inh group
+# (should have size greater than 7x7)
+# input to certain areas of liquid and record voltages/firing rates over time
+# determine manifolds from generated data
