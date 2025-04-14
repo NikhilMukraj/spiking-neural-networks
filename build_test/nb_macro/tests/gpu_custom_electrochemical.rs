75ff1713b0aa98c18670a2a692828573daa39e5a
Nikhiljag <nikhilmukraj@gmail.com>
2025-04-14 17:27:27 -0400

    new tests

--- build_test/nb_macro/tests/gpu_custom_electrochemical.rs
+++ build_test/nb_macro/tests/gpu_custom_electrochemical.rs
@@ -3,6 +3,8 @@
 #[cfg(test)]
 mod test {
     use nb_macro::neuron_builder;
+    use opencl3::device::{get_all_devices, Device, CL_DEVICE_TYPE_GPU};
+    use spiking_neural_networks::error::SpikingNeuralNetworksError;
 
 
     neuron_builder!(r#"
@@ -13,13 +15,45 @@ mod test {
             v = v_reset
         spike_detection: v >= v_th
         on_iteration:
-            dv/dt = (v - e) + i
+            dv/dt = -(v - e) + i
         on_electrochemical_iteration:
             receptors.update_receptor_kinetics(t, dt)
             receptors.set_receptor_currents(v, dt)
-            dv/dt = (v - e) + i
+            dv/dt = -(v - e) + i
             v = (modifier * -receptors.get_receptor_currents(dt, c_m)) + v
             synaptic_neurotransmitters.apply_t_changes()
     [end]
     "#);
+
+    #[test]
+    pub fn test_electrical_kernel_compiles() -> Result<(), SpikingNeuralNetworksError> {
+        let device_id = *get_all_devices(CL_DEVICE_TYPE_GPU)
+            .expect("Could not get GPU devices")
+            .first()
+            .expect("No GPU found");
+        let device = Device::new(device_id);
+
+        let context = Context::from_device(&device).expect("Context::from_device failed");
+
+        match ElectroChemicalIntegrateAndFire::<ApproximateNeurotransmitter, ApproximateReceptor>::iterate_and_spike_electrical_kernel(&context) {
+            Ok(_) => Ok(()),
+            Err(_) => Err(SpikingNeuralNetworksError::GPURelatedError(GPUError::KernelCompileFailure)),
+        }
+    }
+
+    // #[test]
+    // pub fn test_electrochemical_kernel_compiles() -> Result<(), SpikingNeuralNetworksError> {
+    //     let device_id = *get_all_devices(CL_DEVICE_TYPE_GPU)
+    //         .expect("Could not get GPU devices")
+    //         .first()
+    //         .expect("No GPU found");
+    //     let device = Device::new(device_id);
+
+    //     let context = Context::from_device(&device).expect("Context::from_device failed");
+
+    //     match BasicIntegrateAndFire::<ApproximateNeurotransmitter, ApproximateReceptor>::iterate_and_spike_electrochemical_kernel(&context) {
+    //         Ok(_) => Ok(()),
+    //         Err(_) => Err(SpikingNeuralNetworksError::GPURelatedError(GPUError::KernelCompileFailure)),
+    //     }
+    // }
 }
