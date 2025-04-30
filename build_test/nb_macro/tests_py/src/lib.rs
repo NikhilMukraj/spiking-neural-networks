01e291bdf481bbbb903291e6a204a11edd7c8610
Nikhiljag <nikhilmukraj@gmail.com>
2025-04-29 20:49:40 -0400

    new test objects

--- build_test/nb_macro/tests_py/src/lib.rs
+++ build_test/nb_macro/tests_py/src/lib.rs
@@ -2,8 +2,31 @@ use nb_macro::neuron_builder;
 
 
 neuron_builder!(r#"
+[neurotransmitter_kinetics]
+    type: BoundedNeurotransmitterKinetics
+    vars: t_max = 1, c = 0.001, conc = 0
+    on_iteration:
+        [if] is_spiking [then]
+            conc = t_max
+        [else]
+            conc = 0
+        [end]
+
+        t = t + dt * -c * t + conc
+
+        t = min(max(t, 0), t_max)
+[end]
+
+[receptor_kinetics]
+    type: BoundedReceptorKinetics
+    vars: r_max = 1
+    on_iteration:
+        r = min(max(t, 0), r_max)
+[end]
+
 [neuron]
     type: BasicIntegrateAndFire
+    kinetics: BoundedNeurotransmitterKinetics, BoundedReceptorKinetics
     vars: e = 0, v_reset = -75, v_th = -55
     on_spike: 
         v = v_reset
@@ -16,6 +39,7 @@ neuron_builder!(r#"
 #[pymodule]
 fn tests_py(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<PyBasicIntegrateAndFire>()?;
+    m.add_class::<PyBoundedNeurotransmitterKinetics>()?;
 
     Ok(())
 }
