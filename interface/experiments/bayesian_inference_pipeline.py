559c78f1d1748bfe5cafa74e339614f08d1cc02f
Nikhiljag <nikhilmukraj@gmail.com>
2025-05-12 15:33:29 -0400

    bug fixes

--- interface/experiments/bayesian_inference_pipeline.py
+++ interface/experiments/bayesian_inference_pipeline.py
@@ -498,7 +498,7 @@ for current_state in tqdm(all_states):
         else:
             network.connect(c2, e1, lambda x, y: x == y, lambda x, y: current_state['bayesian_to_exc'])
 
-        network.set_dt(parsed['simulation_parameters']['dt'])
+        network.set_dt(parsed_toml['simulation_parameters']['dt'])
         network.parallel = True
 
         network.electrical_synapse = False
