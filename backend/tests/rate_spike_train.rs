6789617790fd70864f24f97b4dd1af7e431aaecc
Nikhiljag <nikhilmukraj@gmail.com>
2025-05-21 18:10:11 -0400

    rate works as expected

--- backend/tests/rate_spike_train.rs
+++ backend/tests/rate_spike_train.rs
@@ -1,6 +1,35 @@
 #[cfg(test)]
 mod test {
+    use spiking_neural_networks::neuron::{iterate_and_spike::{ApproximateNeurotransmitter, IonotropicNeurotransmitterType}, spike_train::{DeltaDiracRefractoriness, RateSpikeTrain, SpikeTrain}};
+
+
     // count number of spikes and compare against given rate and number of iterations
-    // #[test]
-    // fn test_expected_rate() {}
+
+    const ITERATIONS: usize = 10_000;
+
+    #[test]
+    fn test_expected_rate() {
+        let rates = [0, 100, 200, 300, 400, 500];
+
+        for rate in rates {
+            let mut spike_train: RateSpikeTrain<IonotropicNeurotransmitterType, ApproximateNeurotransmitter, DeltaDiracRefractoriness> = RateSpikeTrain {
+                rate: rate as f32,
+                ..Default::default()
+            };
+
+            let mut spikes = 0;
+            for _ in 0..ITERATIONS {
+                let is_spiking = spike_train.iterate();
+                if is_spiking {
+                    spikes += 1;
+                }
+            }
+
+            if rate == 0 {
+                assert_eq!(spikes, 0);
+            } else {
+                assert!((spikes as f32 - (ITERATIONS as f32 / (rate as f32 / 0.1))).abs() <= 1.);
+            }
+        }
+    }
 }
