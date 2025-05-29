7a8d80bec5ece5f2bcc4fd194f47907b7e0a5d8f
Nikhiljag <nikhilmukraj@gmail.com>
2025-05-29 17:32:03 -0400

    new test, current bug seems to only exist on electrical side

--- interface_gpu/lixirnet/tests/networks.py
+++ interface_gpu/lixirnet/tests/networks.py
@@ -270,5 +270,139 @@ class TestCPUGPUImpl(unittest.TestCase):
 
         self.assertTrue(np.abs((np.array(network.get_lattice(e1).history) - np.array(gpu_network.get_lattice(e1).history)).sum()) < 0.1)
         
+    def test_network_chemical_with_spike_trains(self):
+        neuron = ln.IzhikevichNeuron()
+        neuron.gap_conductance = 10
+        neuron.c_m = 25
+
+        glu_neuro = ln.BoundedNeurotransmitterKinetics()
+        exc_neurotransmitters = {ln.DopaGluGABANeurotransmitterType.Glutamate : glu_neuro}
+        glu = ln.GlutamateReceptor()
+        receptors = ln.DopaGluGABA()
+        receptors.insert(ln.DopaGluGABANeurotransmitterType.Glutamate, glu)
+
+        neuron.set_synaptic_neurotransmitters(exc_neurotransmitters)
+        neuron.set_receptors(receptors)
+
+        init_state1 = np.random.uniform(neuron.c, neuron.v_th, (exc_n1, exc_n1))
+        init_state2 = np.random.uniform(neuron.c, neuron.v_th, (exc_n2, exc_n2))
+
+        setup_neuron1 = get_neuron_setup(init_state1)
+        setup_neuron2 = get_neuron_setup(init_state2)
+
+        spike_train = ln.RateSpikeTrain()
+        spike_train.rate = 100
+
+        spike_train.set_synaptic_neurotransmitters(exc_neurotransmitters)
+
+        init_spike_train = np.random.uniform(0, 100, (exc_n1, exc_n1))
+
+        setup_spike_train = get_spike_train_setup(init_spike_train)
+
+        spike_train_lattice = ln.RateSpikeTrainLattice(c1)
+        spike_train_lattice.populate(spike_train, exc_n1, exc_n1)
+        spike_train_lattice.apply_given_position(setup_spike_train)
+        spike_train_lattice.update_grid_history = True
+
+        lattice1 = ln.IzhikevichNeuronLattice(e1)
+        lattice1.populate(neuron, exc_n1, exc_n1)
+        lattice1.apply_given_position(setup_neuron1)
+        lattice1.connect(lambda x, y: x != y, lambda x, y: 5)
+        lattice1.update_grid_history = True
+
+        # lattice2 = ln.IzhikevichNeuronLattice(i1)
+        # lattice2.populate(neuron, exc_n2, exc_n2)
+        # lattice2.apply_given_position(setup_neuron2)
+        # lattice2.connect(lambda x, y: x != y, lambda x, y: 3)
+        # lattice2.update_grid_history = True
+
+        network = ln.IzhikevichNeuronNetwork.generate_network([lattice1], [spike_train_lattice])
+        # network.connect(e1, i1, lambda x, y: x == y, lambda x, y: 5)
+        # network.connect(i1, e1, lambda x, y: x == y, lambda x, y: -3)
+        network.connect(c1, e1, lambda x, y: x == y, lambda x, y: 5)
+        network.electrical_synapse = False
+        network.chemical_synapse = True
+        gpu_network = ln.IzhikevichNeuronNetworkGPU.from_network(network)
+
+        self.assertTrue(network.connecting_weights, gpu_network.connecting_weights)
+
+        network.run_lattices(iterations)
+        gpu_network.run_lattices(iterations)
+
+        np.save(f'cpu_history_{self._testMethodName}_e1.npy', np.array(network.get_lattice(e1).history))
+        np.save(f'gpu_history_{self._testMethodName}_e1.npy', np.array(gpu_network.get_lattice(e1).history))
+
+        # np.save(f'cpu_history_{self._testMethodName}_i1.npy', np.array(network.get_lattice(i1).history))
+        # np.save(f'gpu_history_{self._testMethodName}_i1.npy', np.array(gpu_network.get_lattice(i1).history))
+
+        self.assertTrue(np.abs((np.array(network.get_spike_train_lattice(c1).history) - np.array(gpu_network.get_spike_train_lattice(c1).history)).sum()) < 0.1)
+
+        # self.assertTrue(np.abs((np.array(network.get_lattice(i1).history) - np.array(gpu_network.get_lattice(i1).history)).sum()) < 0.1)
+
+        self.assertTrue(np.abs((np.array(network.get_lattice(e1).history) - np.array(gpu_network.get_lattice(e1).history)).sum()) < 0.1)
+
+    # def test_network_electrical_multiple_lattices_and_spike_train(self):
+    #     neuron = ln.IzhikevichNeuron()
+    #     neuron.gap_conductance = 10
+    #     neuron.c_m = 25
+
+    #     init_state1 = np.random.uniform(neuron.c, neuron.v_th, (exc_n1, exc_n1))
+    #     init_state2 = np.random.uniform(neuron.c, neuron.v_th, (exc_n2, exc_n2))
+
+    #     setup_neuron1 = get_neuron_setup(init_state1)
+    #     setup_neuron2 = get_neuron_setup(init_state2)
+
+    #     spike_train = ln.RateSpikeTrain()
+    #     spike_train.rate = 100
+
+    #     init_spike_train = np.random.uniform(0, 100, (exc_n1, exc_n1))
+
+    #     setup_spike_train = get_spike_train_setup(init_spike_train)
+
+    #     spike_train_lattice = ln.RateSpikeTrainLattice(c1)
+    #     spike_train_lattice.populate(spike_train, exc_n1, exc_n1)
+    #     spike_train_lattice.apply_given_position(setup_spike_train)
+    #     spike_train_lattice.update_grid_history = True
+
+    #     lattice1 = ln.IzhikevichNeuronLattice(e1)
+    #     lattice1.populate(neuron, exc_n1, exc_n1)
+    #     lattice1.apply_given_position(setup_neuron1)
+    #     lattice1.connect(lambda x, y: x != y, lambda x, y: 5)
+    #     lattice1.update_grid_history = True
+
+    #     lattice2 = ln.IzhikevichNeuronLattice(i1)
+    #     lattice2.populate(neuron, exc_n2, exc_n2)
+    #     lattice2.apply_given_position(setup_neuron2)
+    #     lattice2.connect(lambda x, y: x != y, lambda x, y: 3)
+    #     lattice2.update_grid_history = True
+
+    #     network = ln.IzhikevichNeuronNetwork.generate_network([lattice1, lattice2], [spike_train_lattice])
+    #     network.connect(e1, i1, lambda x, y: x == y, lambda x, y: 5)
+    #     network.connect(i1, e1, lambda x, y: x == y, lambda x, y: -3)
+    #     network.connect(c1, e1, lambda x, y: x == y, lambda x, y: 5)
+    #     network.electrical_synapse = True
+    #     network.chemical_synapse = False
+    #     gpu_network = ln.IzhikevichNeuronNetworkGPU.from_network(network)
+
+    #     self.assertTrue(network.connecting_weights, gpu_network.connecting_weights)
+
+    #     network.run_lattices(iterations)
+    #     gpu_network.run_lattices(iterations)
+
+    #     np.save(f'cpu_history_{self._testMethodName}_e1.npy', np.array(network.get_lattice(e1).history))
+    #     np.save(f'gpu_history_{self._testMethodName}_e1.npy', np.array(gpu_network.get_lattice(e1).history))
+
+    #     np.save(f'cpu_history_{self._testMethodName}_i1.npy', np.array(network.get_lattice(i1).history))
+    #     np.save(f'gpu_history_{self._testMethodName}_i1.npy', np.array(gpu_network.get_lattice(i1).history))
+
+    #     self.assertTrue(np.abs((np.array(network.get_spike_train_lattice(c1).history) - np.array(gpu_network.get_spike_train_lattice(c1).history)).sum()) < 0.1)
+
+    #     self.assertTrue(np.abs((np.array(network.get_lattice(i1).history) - np.array(gpu_network.get_lattice(i1).history)).sum()) < 0.1)
+
+    #     self.assertTrue(np.abs((np.array(network.get_lattice(e1).history) - np.array(gpu_network.get_lattice(e1).history)).sum()) < 0.1)
+
+    # def test_network_chemical_multiple_lattices_and_spike_train(self):
+    #     ...
+
 if __name__ == '__main__':
     unittest.main()
