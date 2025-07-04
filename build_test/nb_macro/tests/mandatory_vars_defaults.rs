b9302d16305770674e8d744936cda59a4f2531f5
Nikhiljag <nikhilmukraj@gmail.com>
2025-07-04 19:06:54 -0400

    mandatory vars testing

---
+++ build_test/nb_macro/tests/mandatory_vars_defaults.rs
@@ -0,0 +1,24 @@
+#[cfg(test)]
+mod tests {
+    use nb_macro::neuron_builder;
+
+
+    neuron_builder!(r#"
+        [neuron]
+            type: BasicIntegrateAndFire
+            vars: e = 0, v_reset = -75, v_th = -55, dt = 100
+            on_spike: 
+                v = v_reset
+            spike_detection: v >= v_th
+            on_iteration:
+                dv/dt = (v - e) + i
+        [end]
+    "#);
+
+    #[test]
+    fn test_custom_dt() {
+        let lif = BasicIntegrateAndFire::default_impl();
+
+        assert_eq!(lif.dt, 100.);
+    }
+}
