46699cd677da0b30567aa0ce831901702499cc8e
Nikhiljag <nikhilmukraj@gmail.com>
2025-07-06 18:28:19 -0400

    new test, all tests pass

--- build_test/nb_macro/tests/mandatory_vars_defaults.rs
+++ build_test/nb_macro/tests/mandatory_vars_defaults.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod tests {
     use nb_macro::neuron_builder;
+    use spiking_neural_networks::neuron::iterate_and_spike::IonotropicNeurotransmitterType;
 
 
     neuron_builder!(r#"
@@ -20,6 +21,21 @@ mod tests {
             on_iteration:
                 current = g * (v - e)
         [end]
+
+        [spike_train]
+            type: RateSpikeTrain
+            vars: step = 0, rate = 0, v_resting = 24
+            on_iteration:
+                step += dt
+                [if] rate != 0. && step >= rate [then]
+                    step = 0
+                    current_voltage = v_th
+                    is_spiking = true
+                [else]
+                    current_voltage = v_resting
+                    is_spiking = false
+                [end]
+        [end]
     "#);
 
     #[test]
@@ -34,6 +50,13 @@ mod tests {
         let ion_channel = TestLeak::default();
 
         assert_eq!(ion_channel.current, 10.);
+    }
+
+    #[test]
+    fn test_custom_v_resting() {
+        let spike_train = RateSpikeTrain::default_impl();
+
+        assert_eq!(spike_train.v_resting, 24.);
     }
 
     // test if gpu kernel and conversion works as expected
