85c9e3bf0bc81ce5d58b0af09f95091b968ee7ce
Nikhiljag <nikhilmukraj@gmail.com>
2025-09-12 20:04:03 -0400

    notes

--- interface_gpu/experiments/ring_attractor.py
+++ interface_gpu/experiments/ring_attractor.py
@@ -1,2 +1,2 @@
 # 6 ring system where negative electrical weights are GABA and excitatory weights are glutamate
-# split 3 ring system into 6 rings
+# split 3 ring system into 6 rings based on sign of weight
