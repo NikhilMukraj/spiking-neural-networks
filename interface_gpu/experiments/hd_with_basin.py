994732663c71b956ee223c81e343f1fe7907be23
Nikhiljag <nikhilmukraj@gmail.com>
2025-10-31 23:10:56 -0400

    notes

--- interface_gpu/experiments/hd_with_basin.py
+++ interface_gpu/experiments/hd_with_basin.py
@@ -1,2 +1,2 @@
-# electrochemical hd with basin around certain direction
+# electrochemical hd with basin around certain angle
 # try d1 and d2 action on certain neurons in hd ring exc/inh to bias a certain direction
